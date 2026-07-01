5fe496ae7cb34a922f21136d876f103d066f29ca
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-01 22:27:25 +0530

    Time: 59 ms (5.73%), Space: 136.2 MB (8.77%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -1,32 +1,28 @@
+//Approach-3 (Using 2 pointers)
 class Solution {
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        int m = spells.size();
+        vector<pair<int,int>> sortedSpells;
+        for (int i = 0; i< spells.size(); ++i) {
+            sortedSpells.push_back({ spells[i], i });
+        }
+
+        // Sort the 'spells with index' and 'potions' array in increasing order.
+        sort(sortedSpells.begin(), sortedSpells.end());
+        sort(potions.begin(), potions.end());
+
+        vector<int> answer(spells.size());
+        int m = potions.size();
+        int potionIndex = m - 1;
         
-        int n = potions.size();
         
-        sort(begin(potions), end(potions));
-        
-        int maxPotion = potions[n-1];
-        
-        vector<int> answer;
-        
-        
-        for(int i = 0; i<m; i++) {
-            
-            int spell = spells[i];
-            
-            long long minPotion = ceil((1.0*success)/spell);
-            
-            if(minPotion > maxPotion) {
-                answer.push_back(0);
-                continue;
+        for (const auto& [spell, index] : sortedSpells) {
+          
+            while (potionIndex >= 0 && (long long) spell * potions[potionIndex] >= success) {
+                potionIndex -= 1;
             }
-            
-            int index = lower_bound(begin(potions), end(potions), minPotion) - begin(potions);
-            
-            answer.push_back(n-index);
-            
+            answer[index] = m - (potionIndex + 1);
+          
         }
         
         return answer;
