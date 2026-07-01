2087ee4867731499a353cb27e4b04e48d9e59d33
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-01 20:30:45 +0530

    Time: 0 ms (100%), Space: 15.1 MB (67.38%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -4,10 +4,10 @@ public:
         while(l < r) {
             int mid = l + (r-l)/2;
             
-            if(nums[mid] > nums[r]) {
-                l = mid+1;
-            } else {
+            if(nums[mid]<nums[r]) {
                 r = mid;
+            } else {
+                l = mid+1;
             }
         }
         return r;
