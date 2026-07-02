b9f0f97a42c7aecb8b40d29cc2a236074f9ca7f6
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-02 17:44:49 +0530

    Time: 683 ms (5.04%), Space: 180.1 MB (5.04%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
+    void dijkstra(char &source, unordered_map<char, vector<pair<char, int>>> &adj, vector<vector<long long>> &costMatrix) {
 
-    void FloydWarshall(vector<vector<long long>> &adjMatrix, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+       
+        priority_queue<pair<int, char>, vector<pair<int, char>>, greater<pair<int, char>>> pq;
 
-            for(int i = 0; i < original.size(); i++) {
-                int s = original[i] - 'a';
-                int t = changed[i] - 'a';
+        pq.push({0, source});
 
-                adjMatrix[s][t] = min(adjMatrix[s][t], (long long)cost[i]);//a->b can have more than 1 val
-            }
+        while(!pq.empty()) {
+            int d        = pq.top().first;
+            char adjNode = pq.top().second;
+            pq.pop();
 
-           
-            for (int k = 0; k < 26; ++k) {
-                for (int i = 0; i < 26; ++i) {
-                    for (int j = 0; j < 26; ++j) {
-                        adjMatrix[i][j] = min(adjMatrix[i][j], adjMatrix[i][k] + adjMatrix[k][j]); 
-                    }
+            for(auto &it : adj[adjNode]) {
+                char adjNode = it.first;
+                int cost      = it.second;
+
+                if(costMatrix[source - 'a'][adjNode - 'a'] > d + cost) {
+                    costMatrix[source - 'a'][adjNode - 'a'] = d + cost;
+                    pq.push({d + cost, adjNode});
                 }
             }
+
+        }
+        return;
     }
 
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
-        vector<vector<long long>> adjMatrix(26, vector<long long>(26, INT_MAX));
+        unordered_map<char, vector<pair<char, int>>> adj;
 
-        FloydWarshall(adjMatrix, original, changed, cost); 
+        for(int i = 0; i < original.size(); i++) {
+            adj[original[i]].push_back({changed[i], cost[i]});
+        }
+
+        vector<vector<long long>> costMatrix(26, vector<long long>(26, INT_MAX));
+
+        for(int i = 0; i < source.length(); i++) { 
+            char ch = source[i];
+            dijkstra(ch, adj, costMatrix);
+        }
 
         long long ans = 0;
 
@@ -32,14 +47,14 @@ public:
                 continue;
             }
 
-            if(adjMatrix[source[i] -'a'][target[i] - 'a'] == INT_MAX) {
+            if(costMatrix[source[i] - 'a'][target[i] - 'a'] == INT_MAX) {
                 return -1;
             }
 
-            ans += adjMatrix[source[i] -'a'][target[i] - 'a'];
+            ans += costMatrix[source[i] - 'a'][target[i] - 'a'];
+
         }
 
         return ans;
-
     }
 };
