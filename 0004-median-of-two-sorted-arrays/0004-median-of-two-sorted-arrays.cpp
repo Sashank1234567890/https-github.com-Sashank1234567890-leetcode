34ace382744373792e065af3e9fad52357bf79d1
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-03 13:27:00 +0530

    Time: 0 ms (100%), Space: 95 MB (94.79%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,36 +1,49 @@
 class Solution {
 public:
+
+    int kth(vector<int>& a, int i,
+            vector<int>& b, int j,
+            int k) {
+
+        // One array finished
+        if(i >= a.size())
+            return b[j + k - 1];
+
+        if(j >= b.size())
+            return a[i + k - 1];
+
+        // Smallest element
+        if(k == 1)
+            return min(a[i], b[j]);
+
+        int half = k / 2;
+
+        int mid1 = INT_MAX;
+        int mid2 = INT_MAX;
+
+        if(i + half - 1 < a.size())
+            mid1 = a[i + half - 1];
+
+        if(j + half - 1 < b.size())
+            mid2 = b[j + half - 1];
+
+        if(mid1 < mid2)
+            return kth(a, i + half, b, j, k - half);
+
+        return kth(a, i, b, j + half, k - half);
+    }
+
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size() > nums2.size())
-            return findMedianSortedArrays(nums2, nums1);
-        
-        int m = nums1.size();
-        int n = nums2.size();
-        
-        int low = 0, high = m;
-        while(low <= high) {
-            
-            int Px = low + (high-low)/2;
-            int Py = (m+n+1)/2 - Px;
-            
-            int x1  = (Px == 0) ? INT_MIN : nums1[Px-1];
-            int x3 = (Px == m) ? INT_MAX : nums1[Px];
-            
-            int x2  = (Py == 0) ? INT_MIN : nums2[Py-1];
-            int x4 = (Py == n) ? INT_MAX : nums2[Py];
-            
-            if(x1 <= x4 && x2 <= x3) {
-                if((m+n)%2 == 0)
-                    return (max(x1, x2) + min(x3, x4))/2.0;
-                
-                return max(x1, x2);
-            } else if(x1 > x4) {
-                high = Px-1;
-            } else {
-                low = Px+1;
-            }
-        }
-        
-        return -1;
+
+        int total = nums1.size() + nums2.size();
+
+        if(total % 2)
+            return kth(nums1, 0, nums2, 0, total / 2 + 1);
+
+        int left = kth(nums1, 0, nums2, 0, total / 2);
+
+        int right = kth(nums1, 0, nums2, 0, total / 2 + 1);
+
+        return (left + right) / 2.0;
     }
 };
