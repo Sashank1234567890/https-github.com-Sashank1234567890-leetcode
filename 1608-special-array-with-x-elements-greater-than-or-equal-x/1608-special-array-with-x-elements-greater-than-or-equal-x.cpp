f8fb37e87770ddd40ff76e4118218dfcfb5034f3
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-05 17:21:10 +0530

    Time: 0 ms (100%), Space: 11.9 MB (6.61%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-
     int specialArray(vector<int>& nums) {
-        sort(begin(nums), end(nums));
         int n = nums.size();
-        
-        int l = 0, r = n;
-        while(l <= r) {
-            int mid_x = l + (r-l)/2;
 
-            int i = lower_bound(begin(nums), end(nums), mid_x) - begin(nums);
+        /*
+            Every number larger than N is useless for our purpose,
+            and we can treat those numbers as N. 
+        */
+        vector<int> freq(n+1);
 
-            if(n-i == mid_x) {
-                return mid_x;
-            } else if(n-i > mid_x) {
-                l = mid_x+1;
-            } else {
-                r = mid_x-1;
-            }
+        for(int i = 0; i < n; i++) {
+            freq[min(n, nums[i])]++;
         }
 
+        int c_sum = 0;
+        for(int i = n; i >= 0; i--) {
+            c_sum += freq[i];
+            if(i == c_sum) {
+                return i;
+            }
+        }
+        
         return -1;
     }
 };
