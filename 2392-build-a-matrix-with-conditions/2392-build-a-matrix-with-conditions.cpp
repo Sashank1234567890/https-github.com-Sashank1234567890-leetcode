8363345799b70e375b2eaaa23b91d562d6cb574b
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-05 15:08:56 +0530

    Time: 9 ms (76.16%), Space: 61.3 MB (67.62%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
@@ -1,73 +1,65 @@
 class Solution {
 public:
-
-    vector<int> topoSort(int k, vector<vector<int>>& conditions) {
-
-        vector<vector<int>> adj(k + 1);
-        vector<int> indegree(k + 1);
-
-        for(auto &e : conditions) {
-            adj[e[0]].push_back(e[1]);
-            indegree[e[1]]++;
-        }
-
-        queue<int> q;
-
-        for(int i = 1; i <= k; i++) {
-            if(indegree[i] == 0)
-                q.push(i);
-        }
-
+    
+    vector<int> topoSort(vector<vector<int>>& edges, int n) {
+        vector<vector<int>> adj(n + 1);
+        vector<int> indegree(n + 1);
         vector<int> order;
-
-        while(!q.empty()) {
-
-            int u = q.front();
-            q.pop();
-
-            order.push_back(u);
-
-            for(int v : adj[u]) {
-
-                indegree[v]--;
-
-                if(indegree[v] == 0)
-                    q.push(v);
+        for (vector<int>& edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+        
+        queue<int> que;
+        int count = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if (indegree[i] == 0) {
+                que.push(i);
+                count++;
             }
         }
+ 
+        while(!que.empty()) {
+	        int u = que.front();
+	        order.push_back(u);
+	        que.pop();
+	        
+	        for(int &v : adj[u]) {
+	            indegree[v]--;
+	            
+	            if(indegree[v] == 0) {
+	                que.push(v);
+                    count++;
+	            }
+	            
+	        }
+	    }
 
-        if(order.size() != k)
-            return {};
+        if (count != n) 
+            return {}; 
 
         return order;
     }
 
     vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
+        vector<int> orderRows    = topoSort(rowConditions, k);
+        vector<int> orderColumns = topoSort(colConditions, k);
 
-        vector<int> rowOrder = topoSort(k, rowConditions);
-
-        if(rowOrder.empty())
+      
+        if (orderRows.empty() or orderColumns.empty())
             return {};
 
-        vector<int> colOrder = topoSort(k, colConditions);
-
-        if(colOrder.empty())
-            return {};
-
-        vector<int> rowPos(k + 1);
-        vector<int> colPos(k + 1);
-
-        for(int i = 0; i < k; i++) {
-            rowPos[rowOrder[i]] = i;
-            colPos[colOrder[i]] = i;
+        vector<vector<int>> matrix(k, vector<int>(k, 0));
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < k; j++) {
+                if (orderRows[i] == orderColumns[j]) {
+                    matrix[i][j] = orderRows[i];
+                }
+            }
         }
-
-        vector<vector<int>> ans(k, vector<int>(k, 0));
-
-        for(int i = 1; i <= k; i++) {
-            ans[rowPos[i]][colPos[i]] = i;
-        }
-
-        return ans;
+        return matrix;
     }
 };
