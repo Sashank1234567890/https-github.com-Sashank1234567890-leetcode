36e5a391b5d1081a6140c2ef7377bae17d0690c3
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-05 13:56:14 +0530

    Time: 486 ms (67.19%), Space: 224.7 MB (65.61%) - LeetHub

--- 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.cpp
+++ 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.cpp
@@ -1,61 +1,58 @@
 class Solution {
 public:
-    int m, n;
-    int t[501][501][3];
-
-    int solve(vector<vector<int>>& coins, int i, int j, int neu) {
-
-        if(i >= m || j >= n)
-            return INT_MIN;
-
-        if(i == m-1 && j == n-1) {
-            if(coins[i][j] >= 0)
-                return coins[i][j];
-
-            if(neu > 0)
-                return 0;
-
-            return coins[i][j];
-        }
-
-        if(t[i][j][neu] != INT_MIN)
-            return t[i][j][neu];
-
-        int ans = INT_MIN;
-
-        // Don't use neutralizer
-        int down = solve(coins, i+1, j, neu);
-        int right = solve(coins, i, j+1, neu);
-
-        if(down != INT_MIN)
-            ans = max(ans, coins[i][j] + down);
-
-        if(right != INT_MIN)
-            ans = max(ans, coins[i][j] + right);
-
-        // Use neutralizer
-        if(coins[i][j] < 0 && neu > 0) {
-
-            down = solve(coins, i+1, j, neu-1);
-            right = solve(coins, i, j+1, neu-1);
-
-            ans = max(ans, down);
-            ans = max(ans, right);
-        }
-
-        return t[i][j][neu] = ans;
-    }
-
     int maximumAmount(vector<vector<int>>& coins) {
 
-        m = coins.size();
-        n = coins[0].size();
+        int m = coins.size();
+        int n = coins[0].size();
 
-        for(int i = 0; i < 501; i++)
-            for(int j = 0; j < 501; j++)
-                for(int k = 0; k < 3; k++)
-                    t[i][j][k] = INT_MIN;
+        vector<vector<vector<int>>> dp(
+            m, vector<vector<int>>(n, vector<int>(3, INT_MIN)));
 
-        return solve(coins, 0, 0, 2);
+        for(int neu = 0; neu <= 2; neu++) {
+
+            if(coins[m-1][n-1] >= 0)
+                dp[m-1][n-1][neu] = coins[m-1][n-1];
+            else {
+                dp[m-1][n-1][neu] = coins[m-1][n-1];
+
+                if(neu > 0)
+                    dp[m-1][n-1][neu] = 0;
+            }
+        }
+
+        for(int i = m-1; i >= 0; i--) {
+
+            for(int j = n-1; j >= 0; j--) {
+
+                if(i == m-1 && j == n-1)
+                    continue;
+
+                for(int neu = 0; neu <= 2; neu++) {
+
+                    int ans = INT_MIN;
+
+                    // Don't use neutralizer
+                    if(i+1 < m && dp[i+1][j][neu] != INT_MIN)
+                        ans = max(ans, coins[i][j] + dp[i+1][j][neu]);
+
+                    if(j+1 < n && dp[i][j+1][neu] != INT_MIN)
+                        ans = max(ans, coins[i][j] + dp[i][j+1][neu]);
+
+                    // Use neutralizer
+                    if(coins[i][j] < 0 && neu > 0) {
+
+                        if(i+1 < m)
+                            ans = max(ans, dp[i+1][j][neu-1]);
+
+                        if(j+1 < n)
+                            ans = max(ans, dp[i][j+1][neu-1]);
+                    }
+
+                    dp[i][j][neu] = ans;
+                }
+            }
+        }
+
+        return dp[0][0][2];
     }
 };
