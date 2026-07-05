47766b060792242a12b0b67af8b52004da8d1f10
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-05 18:47:28 +0530

    Time: 529 ms (6.98%), Space: 31.7 MB (6.9%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     int smallestDistancePair(vector<int>& nums, int k) {
         int n = nums.size();
-        int maxEl = *max_element(begin(nums), end(nums));
-        vector<int> vec(maxEl + 1, 0);
 
-       
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
+        int mn = *min_element(nums.begin(), nums.end());
+        int mx = *max_element(nums.begin(), nums.end());
+
+        vector<int> vec(mx - mn + 1, 0);
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
                 int d = abs(nums[i] - nums[j]);
                 vec[d]++;
             }
         }
 
-
-        for (int d = 0; d <= maxEl; d++) {
+        for(int d = 0; d <= mx - mn; d++) {
             k -= vec[d];
-            if (k <= 0) {
-                return d; 
-            }
+            if(k <= 0)
+                return d;
         }
+
         return -1;
     }
 };
-
