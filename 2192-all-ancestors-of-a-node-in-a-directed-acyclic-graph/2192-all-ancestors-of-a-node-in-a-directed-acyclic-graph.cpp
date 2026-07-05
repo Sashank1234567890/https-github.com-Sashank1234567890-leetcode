716fa33c2ea2f5d9998990f65ab1f37d0b8d0472
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-05 14:35:14 +0530

    Time: 323 ms (49.76%), Space: 150.9 MB (34.3%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
 
-        vector<set<int>> st(n);
+        vector<unordered_set<int>> st(n);
 
-        unordered_map<int, vector<int>> adj;
-        vector<int> in(n);
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n);
 
         for(auto &e : edges) {
             int u = e[0];
             int v = e[1];
 
             adj[u].push_back(v);
-            in[v]++;
+            indegree[v]++;
         }
 
         queue<int> q;
 
         for(int i = 0; i < n; i++) {
-            if(in[i] == 0)
+            if(indegree[i] == 0)
                 q.push(i);
         }
 
@@ -29,14 +29,14 @@ public:
 
             for(int v : adj[u]) {
 
-                in[v]--;
+                indegree[v]--;
 
                 for(int x : st[u])
                     st[v].insert(x);
 
                 st[v].insert(u);
 
-                if(in[v] == 0)
+                if(indegree[v] == 0)
                     q.push(v);
             }
         }
@@ -44,8 +44,11 @@ public:
         vector<vector<int>> result(n);
 
         for(int i = 0; i < n; i++) {
+
             for(int x : st[i])
                 result[i].push_back(x);
+
+            sort(result[i].begin(), result[i].end());
         }
 
         return result;
