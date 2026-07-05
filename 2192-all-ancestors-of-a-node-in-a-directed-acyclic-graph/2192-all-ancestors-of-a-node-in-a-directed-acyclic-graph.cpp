909854c15c9caed393f3226aadf648c15c1d392b
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-05 14:39:28 +0530

    Time: 94 ms (66.26%), Space: 75.2 MB (77.75%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,34 +1,47 @@
 class Solution {
 public:
 
-    void DFS(int ancestor, unordered_map<int, vector<int>>& adj, int currNode, vector<vector<int>>& result) {
+    void DFS(int u, unordered_map<int, vector<int>>& adj, vector<bool>& visited) {
+        visited[u] = true;
 
-        for(int &ngbr : adj[currNode]) {
-            if(result[ngbr].empty() || result[ngbr].back() != ancestor) { //to avoid duplicate entry
-                result[ngbr].push_back(ancestor);
-                DFS(ancestor, adj, ngbr, result);
+        for(int &v : adj[u]) {
+            if(visited[v] != true) {
+                DFS(v, adj, visited);
             }
         }
-
     }
 
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> result(n);
+        vector<vector<int>> result;
         unordered_map<int, vector<int>> adj;
 
-        for(vector<int>& vec : edges) {
+        for(vector<int>& vec : edges) { 
             int u = vec[0];
             int v = vec[1];
 
-            adj[u].push_back(v); 
+            adj[v].push_back(u); //v --> u
         }
 
 
-        for(int i = 0; i < n; i++) {
-            int ancestor = i;
-            DFS(ancestor, adj, i, result);
+        for(int u = 0; u < n; u++) {
+            vector<int> ancestors;
+            vector<bool> visited(n, false);
+            DFS(u, adj, visited);
+
+            for(int i = 0; i < n; i++) {
+                if(i == u) continue;
+                
+                if(visited[i] == true) {
+                    ancestors.push_back(i);
+                }
+            }
+
+            result.push_back(ancestors);
         }
 
+
+
+        
         return result;
     }
 };
