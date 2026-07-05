0bb20f22559505f25109536bfa5be550534619b0
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-05 14:38:34 +0530

    Time: 62 ms (83.41%), Space: 65.5 MB (94.44%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,54 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
 
-        vector<unordered_set<int>> st(n);
+    void DFS(int ancestor, unordered_map<int, vector<int>>& adj, int currNode, vector<vector<int>>& result) {
 
-        vector<vector<int>> adj(n);
-        vector<int> indegree(n);
-
-        for(auto &e : edges) {
-            int u = e[0];
-            int v = e[1];
-
-            adj[u].push_back(v);
-            indegree[v]++;
-        }
-
-        queue<int> q;
-
-        for(int i = 0; i < n; i++) {
-            if(indegree[i] == 0)
-                q.push(i);
-        }
-
-        while(!q.empty()) {
-
-            int u = q.front();
-            q.pop();
-
-            for(int v : adj[u]) {
-
-                indegree[v]--;
-
-                for(int x : st[u])
-                    st[v].insert(x);
-
-                st[v].insert(u);
-
-                if(indegree[v] == 0)
-                    q.push(v);
+        for(int &ngbr : adj[currNode]) {
+            if(result[ngbr].empty() || result[ngbr].back() != ancestor) { //to avoid duplicate entry
+                result[ngbr].push_back(ancestor);
+                DFS(ancestor, adj, ngbr, result);
             }
         }
 
+    }
+
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
         vector<vector<int>> result(n);
+        unordered_map<int, vector<int>> adj;
+
+        for(vector<int>& vec : edges) {
+            int u = vec[0];
+            int v = vec[1];
+
+            adj[u].push_back(v); 
+        }
+
 
         for(int i = 0; i < n; i++) {
-
-            for(int x : st[i])
-                result[i].push_back(x);
-
-            sort(result[i].begin(), result[i].end());
+            int ancestor = i;
+            DFS(ancestor, adj, i, result);
         }
 
         return result;
