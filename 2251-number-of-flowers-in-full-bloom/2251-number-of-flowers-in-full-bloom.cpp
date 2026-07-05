0f808f23afbf94d346ec9ad06b621ca1a490e04a
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-05 16:26:07 +0530

    Time: 30 ms (91.51%), Space: 83.2 MB (97.52%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
     vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
-
-        map<int, int> diff;
-
-        for(auto &f : flowers) {
-            diff[f[0]]++;
-            diff[f[1] + 1]--;
+        int m = flowers.size();
+        int n = people.size();
+        
+        vector<int> result(n);
+        
+        vector<int> start_time(m);
+        vector<int> end_time(m);
+        
+        for(int i = 0; i<m; i++) {
+            start_time[i] = flowers[i][0];
+            end_time[i]   = flowers[i][1];
         }
+        
+        sort(begin(start_time), end(start_time));
+        sort(begin(end_time), end(end_time));
+        
+        for(int i = 0; i<n; i++) {
+           
+            int bloomed_already = upper_bound(begin(start_time), end(start_time), people[i]) - begin(start_time);
+            
+            int died_already    = lower_bound(begin(end_time), end(end_time), people[i]) - begin(end_time);
 
-        vector<pair<int,int>> queries;
-
-        for(int i = 0; i < people.size(); i++) {
-            queries.push_back({people[i], i});
+            result[i] = bloomed_already - died_already;
+            
         }
-
-        sort(queries.begin(), queries.end());
-
-        vector<int> ans(people.size());
-
-        int bloom = 0;
-
-        auto it = diff.begin();
-
-        for(auto &[time, idx] : queries) {
-
-            while(it != diff.end() && it->first <= time) {
-                bloom += it->second;
-                it++;
-            }
-
-            ans[idx] = bloom;
-        }
-
-        return ans;
+        
+        return result;
     }
 };
