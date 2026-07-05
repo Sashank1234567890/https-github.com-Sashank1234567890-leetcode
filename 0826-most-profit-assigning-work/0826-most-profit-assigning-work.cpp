f43fd61097280dfa0f98b0f11ad525e78279acb9
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-05 17:57:47 +0530

    Time: 9 ms (73.44%), Space: 41.6 MB (72.89%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -4,29 +4,41 @@ public:
         int n = difficulty.size();
         int m = worker.size();
 
-        priority_queue<pair<int, int>> pq; //max heap of pairs
-
+        vector<pair<int, int>> vec;
         for(int i = 0; i < n; i++) {
-            int diff = difficulty[i];
-            int prof = profit[i];
-
-            pq.push({prof, diff});
+            vec.push_back({difficulty[i], profit[i]});
         }
 
-        sort(begin(worker), end(worker), greater<int>()); 
+        sort(begin(vec), end(vec));
 
-        int i = 0;
+        
+        for(int i = 1; i < vec.size(); i++) {
+            vec[i].second = max(vec[i].second, vec[i-1].second);
+        }
+
+
         int totalProfit = 0;
-        while(i < m && !pq.empty()) {
-            if(pq.top().second > worker[i]) {
-                pq.pop();
-            } else {
-                totalProfit += pq.top().first;
-                i++;
+        for(int i = 0; i < m; i++) {
+
+            int workerDiffLevel = worker[i];
+
+           
+            int l = 0, r = vec.size()-1;
+            int maxProfit = 0;
+            while(l <= r) {
+                int mid = l + (r-l)/2;
+                if(vec[mid].first <= workerDiffLevel) {
+                    maxProfit = max(maxProfit, vec[mid].second);
+                    l = mid+1;
+                } else {
+                    r = mid-1;
+                }
             }
+
+            totalProfit += maxProfit;
+
         }
 
         return totalProfit;
-        
     }
 };
