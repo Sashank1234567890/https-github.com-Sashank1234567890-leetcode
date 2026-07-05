2d11ced399ac4403b27a1e93aef5fbf881347851
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-05 17:58:45 +0530

    Time: 0 ms (100%), Space: 41.7 MB (60.11%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -3,7 +3,7 @@ public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
         int n = difficulty.size();
         int m = worker.size();
-
+        
         vector<pair<int, int>> vec;
         for(int i = 0; i < n; i++) {
             vec.push_back({difficulty[i], profit[i]});
@@ -11,28 +11,17 @@ public:
 
         sort(begin(vec), end(vec));
 
-        
-        for(int i = 1; i < vec.size(); i++) {
-            vec[i].second = max(vec[i].second, vec[i-1].second);
-        }
-
+        sort(begin(worker), end(worker));
 
         int totalProfit = 0;
+        int j = 0; 
+        int maxProfit = 0;
+    
         for(int i = 0; i < m; i++) {
-
-            int workerDiffLevel = worker[i];
-
-           
-            int l = 0, r = vec.size()-1;
-            int maxProfit = 0;
-            while(l <= r) {
-                int mid = l + (r-l)/2;
-                if(vec[mid].first <= workerDiffLevel) {
-                    maxProfit = max(maxProfit, vec[mid].second);
-                    l = mid+1;
-                } else {
-                    r = mid-1;
-                }
+            
+            while(j < n && worker[i] >= vec[j].first) {
+                maxProfit = max(maxProfit, vec[j].second);
+                j++;
             }
 
             totalProfit += maxProfit;
