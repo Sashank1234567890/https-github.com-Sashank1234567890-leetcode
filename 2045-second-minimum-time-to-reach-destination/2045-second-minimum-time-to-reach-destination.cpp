f41fc523c1048e8cef30dc2483f7542b4208ba05
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-06 15:54:47 +0530

    Time: 113 ms (98.05%), Space: 186.7 MB (78.98%) - LeetHub

--- 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.cpp
+++ 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.cpp
@@ -3,7 +3,7 @@ public:
     #define P pair<int, int>
 
     int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
-        unordered_map<int, vector<int>> adj(n + 1);
+        vector<vector<int>> adj(n + 1);
         for (auto& edge : edges) {
             int u = edge[0];
             int v = edge[1];
@@ -13,31 +13,31 @@ public:
 
         vector<int> d1(n + 1, INT_MAX);
         vector<int> d2(n + 1, INT_MAX);
-        priority_queue<P, vector<P>, greater<P>> pq;
-        pq.push({0, 1});
+        queue<P> que;
+        que.push({1, 1}); //Visited node 1 once
         d1[1] = 0;
 
-        while (!pq.empty()) {
-            auto [timePassed, node] = pq.top();
-            pq.pop();
+        while (!que.empty()) {
+            auto [node, freq] = que.front();
+            que.pop();
 
-            if (d2[n] != INT_MAX && node == n) { 
+            int timePassed = (freq == 1) ? d1[node] : d2[node];
+            if (d2[n] != INT_MAX && node == n) { //We reached n 2nd time means it's the second minimum
                 return d2[n];
             }
 
             int mult = timePassed / change;
-            if(mult % 2 == 1) { //red
-                timePassed = change * (mult + 1); 
+            if(mult % 2 == 1) { //Red
+                timePassed = change * (mult + 1); //to set green
             }
 
             for (auto& nbr : adj[node]) {
-                if (d1[nbr] > timePassed + time) { 
-                    d2[nbr] = d1[nbr];
+                if(d1[nbr] == INT_MAX) {
                     d1[nbr] = timePassed + time;
-                    pq.push({timePassed + time, nbr});
-                } else if (d2[nbr] > timePassed + time && d1[nbr] != timePassed + time) {
+                    que.push({nbr, 1});
+                } else if(d2[nbr] == INT_MAX && d1[nbr] != timePassed + time) {
                     d2[nbr] = timePassed + time;
-                    pq.push({timePassed + time, nbr});
+                    que.push({nbr, 2});
                 }
             }
         }
