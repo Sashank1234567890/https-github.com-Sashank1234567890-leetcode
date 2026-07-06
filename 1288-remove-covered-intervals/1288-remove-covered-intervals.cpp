f1a512c81285c1666d17879589dc4fabaec62d4f
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-06 10:18:26 +0530

    Time: 75 ms (6.09%), Space: 33.6 MB (5.3%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& time) {
+    
+        int n=time.size();
+        auto comp=[&](auto a,auto b){
+            if(a[0]==b[0])
+                return b[1]<a[1];
+             return a[0]<b[0];   
+        };
+        sort(begin(time),end(time),comp);
+        vector<vector<int>>result;
+        result.push_back(time[0]);
+        for(int i=1;i<n;i++){
+            if(time[i][0]>=result.back()[0]&&result.back()[1]>=time[i][1]){
+                continue;
+            }
+            result.push_back(time[i]);
+        }
+
+       
+   return result.size(); }
+};
