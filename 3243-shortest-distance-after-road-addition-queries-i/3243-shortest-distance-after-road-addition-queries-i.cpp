88011f3da01fa8c2798974cae238983ac7e69544
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-07 11:25:40 +0530

    Time: 138 ms (56.33%), Space: 114.9 MB (71.4%) - LeetHub

--- 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.cpp
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.cpp
@@ -1,52 +1,49 @@
 class Solution {
 public:
-    #define P pair<int, int>
-    unordered_map<int, vector<P>> adj;
+    unordered_map<int, vector<int>> adj;
 
-    int dijkstra(int n) {
-        vector<int> result(n, INT_MAX);
-        result[0] = 0;
-        priority_queue<P, vector<P>, greater<P>> pq;
-        pq.push({0, 0});
+    int bfs(int n) {
+        queue<int> que;
+        que.push(0);
+        vector<bool> visited(n, false);
+        visited[0] = true;
 
-        while (!pq.empty()) {
-            int d    = pq.top().first;    // Distance of current node
-            int node = pq.top().second; // Current node
-            pq.pop();
-            
-            if (node == n - 1)
-                return result[n - 1];
-            
-            if (d > result[node])
-                continue;
+        int level = 0;
+        while (!que.empty()) {
+            int size = que.size();
+            while (size--) {
+                int node = que.front();
+                que.pop();
 
-            for (P &p : adj[node]) {
-                int adjNode = p.first;
-                int dist    = p.second;
-                if (d + dist < result[adjNode]) {
-                    result[adjNode] = d + dist;
-                    pq.push({d + dist, adjNode});
+                if (node == n - 1) {
+                    return level; // Found the destination node
+                }
+
+                for (int neighbor : adj[node]) {
+                    if (!visited[neighbor]) {
+                        que.push(neighbor);
+                        visited[neighbor] = true;
+                    }
                 }
             }
+            level++;
         }
-        return result[n - 1];
+        return -1; // If destination node is unreachable
     }
 
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
-        vector<int> result;
+        adj.clear();
 
-        for (int i = 0; i < n - 1; ++i) {
-            adj[i].push_back({i + 1, 1});
+        for (int i = 0; i < n - 1; i++) {
+            adj[i].push_back(i + 1);
         }
 
-        for (auto query : queries) {
-            int u = query[0];
-            int v = query[1];
-            adj[u].push_back({v, 1});
-            int d = dijkstra(n);
-            result.push_back(d);
+        int k = queries.size();
+        vector<int> res(k);
+        for (int i = 0; i < k; i++) {
+            adj[queries[i][0]].push_back(queries[i][1]);
+            res[i] = bfs(n);
         }
-
-        return result;
+        return res;
     }
 };
