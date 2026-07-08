750d95470038991ecf6eab3fc595776e102da42b
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-08 11:30:42 +0530

    Time: 36 ms (74.61%), Space: 155.2 MB (67.36%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -1,86 +1,66 @@
 class Solution {
 public:
-    static const int MOD = 1e9 + 7;
-
-    struct Node {
-        long long val;   
-        long long sum;  
-        int cnt;        
-
-        Node(long long v = 0, long long s = 0, int c = 0) {
-            val = v;
-            sum = s;
-            cnt = c;
-        }
-    };
-
-    vector<Node> seg;
-    vector<long long> pow10;
-
-    Node merge(Node &L, Node &R) {
-        Node res;
-        res.cnt = L.cnt + R.cnt;
-        res.sum = L.sum + R.sum;
-        res.val = (L.val * pow10[R.cnt] + R.val) % MOD;
-        return res;
-    }
-
-    void build(int idx, int l, int r, string &s) {
-        if (l == r) {
-            if (s[l] == '0')
-                seg[idx] = Node(0, 0, 0);
-            else {
-                int d = s[l] - '0';
-                seg[idx] = Node(d, d, 1);
-            }
-            return;
-        }
-
-        int mid = (l + r) / 2;
-
-        build(2 * idx + 1, l, mid, s);
-        build(2 * idx + 2, mid + 1, r, s);
-
-        seg[idx] = merge(seg[2 * idx + 1], seg[2 * idx + 2]);
-    }
-
-    Node query(int idx, int l, int r, int ql, int qr) {
-        if (r < ql || l > qr)
-            return Node();
-
-        if (ql <= l && r <= qr)
-            return seg[idx];
-
-        int mid = (l + r) / 2;
-
-        Node left = query(2 * idx + 1, l, mid, ql, qr);
-        Node right = query(2 * idx + 2, mid + 1, r, ql, qr);
-
-        return merge(left, right);
-    }
+    int MOD = 1e9+7;
+    typedef long long ll;
 
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
+        int n = s.length();
 
-        int n = s.size();
+        vector<int> nonZeroCount(n, 0);    // non-zero digits count in s[0..i]
+        vector<ll> numberUpTo(n, 0);      // number formed from non-zero digits in s[0..i]
+        vector<ll> digitSumUpTo(n, 0);    // digit sum of s[0..i]
+        vector<ll> pow10(n + 1, 0);  // 10^i
 
-        seg.assign(4 * n, Node());
-
-        pow10.assign(n + 1, 1);
-
+        pow10[0] = 1;
         for (int i = 1; i <= n; i++)
             pow10[i] = (pow10[i - 1] * 10) % MOD;
 
-        build(0, 0, n - 1, s);
 
-        vector<int> ans;
+        nonZeroCount[0]      = (s[0] != '0') ? 1 : 0;
+        numberUpTo[0]   = s[0] - '0';
+        digitSumUpTo[0] = s[0] - '0';
 
-        for (auto &q : queries) {
-
-            Node cur = query(0, 0, n - 1, q[0], q[1]);
-
-            ans.push_back((cur.val * cur.sum) % MOD);
+        for (int i = 1; i < n; i++) {
+            int digit = s[i] - '0';
+            nonZeroCount[i] = nonZeroCount[i - 1] + (digit != 0 ? 1 : 0);
         }
 
-        return ans;
+        for (int i = 1; i < n; i++) {
+            int digit = s[i] - '0';
+            if (digit != 0)
+                numberUpTo[i] = (numberUpTo[i - 1] * 10 + digit) % MOD;
+            else
+                numberUpTo[i] = numberUpTo[i - 1];
+        }
+
+        for (int i = 1; i < n; i++) {
+            digitSumUpTo[i] = digitSumUpTo[i - 1] + (s[i] - '0');
+        }
+
+        int q = queries.size();
+        vector<int> result(q);
+
+        for (int i = 0; i < q; i++) {
+            int l  = queries[i][0];
+            int r = queries[i][1];
+
+            int startCount = (l == 0) ? 0 : nonZeroCount[l - 1];
+            ll numBefore   = (l == 0) ? 0 : numberUpTo[l - 1];
+
+            int endCount = nonZeroCount[r];
+            int subStrLen = endCount - startCount;
+
+            if (subStrLen == 0) {
+                result[i] = 0;
+                continue;
+            }
+
+            ll x   = (numberUpTo[r] - (numBefore * pow10[subStrLen] % MOD) + MOD) % MOD;
+            ll sum = digitSumUpTo[r] - ((l == 0) ? 0 : digitSumUpTo[l - 1]);
+
+            result[i] = (int)((x * sum) % MOD);
+        }
+
+        return result;
     }
 };
