e7be07f753f17f0e1df5cac22b1936653c162b5f
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-09 17:26:26 +0530

    Time: 228 ms (7.69%), Space: 33.3 MB (68.67%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -1,52 +1,55 @@
-class Solution
-{
-    public:
-        vector<bool> checkIfPrerequisite(int n, vector<vector < int>> &prerequisites, vector< vector< int>> &queries)
-        {
+class Solution {
+public:
+    // Perform DFS and mark prerequisites for each node
+    bool dfs(unordered_map<int, vector<int>>& adj, vector<bool>& visited, int src, int target) {
+        visited[src] = true;
 
-            vector<vector < int>> adj(n);
-            vector<int> indegree(n, 0);
+        if (src == target) {
+            return true;
+        }
 
-            for (auto &e: prerequisites)
-            {
-                adj[e[0]].push_back(e[1]);
-                indegree[e[1]]++;
+        bool isPrerequisite = false;
+        for (auto adjNode : adj[src]) {
+            if (!visited[adjNode]) {
+                isPrerequisite = isPrerequisite || dfs(adj, visited, adjNode, target);
             }
+        }
+        return isPrerequisite;
+    }
 
-            queue<int> q;
-
-            for (int i = 0; i < n; i++)
-            {
-                if (indegree[i] == 0)
-                    q.push(i);
-            }
-
-            vector<unordered_set < int>> pre(n);
-
-            while (!q.empty())
-            {
-
-                int u = q.front();
-                q.pop();
-
-                for (int v: adj[u])
-                {
-
-                    pre[v].insert(u);
-                    pre[v].insert(pre[u].begin(), pre[u].end());
-
-                    indegree[v]--;
-
-                    if (indegree[v] == 0)
-                        q.push(v);
+    void preprocess(int numCourses, unordered_map<int, vector<int>>& adjList, vector<vector<bool>>& isPrerequisite) {
+        for (int u = 0; u < numCourses; u++) {
+            for (int v = 0; v < numCourses; v++) {
+                if (u != v) {
+                    vector<bool> visited(numCourses, false);
+                    if (dfs(adjList, visited, u, v)) {
+                        isPrerequisite[u][v] = true;
+                    }
                 }
             }
-
-            vector<bool> ans;
-
-            for (auto &q: queries)
-                ans.push_back(pre[q[1]].count(q[0]));
-
-            return ans;
         }
+    }
+
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+        unordered_map<int, vector<int>> adjList;
+        for (auto edge : prerequisites) {
+            adjList[edge[0]].push_back(edge[1]);
+        }
+
+        vector<vector<bool>> isPrerequisite(numCourses, vector<bool>(numCourses, false));
+        preprocess(numCourses, adjList, isPrerequisite);
+
+        int Q = queries.size();
+        vector<bool> result(Q);
+
+        for(int i = 0; i < Q; i++) {
+            int u = queries[i][0]; //source.
+            int v = queries[i][1]; //Dest.
+
+            result[i] = isPrerequisite[u][v];
+        }
+
+        return result;
+    }
 };
+
