c7de3403f3ad6b7091b990a2ffe1b4ccd895b94b
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-11 10:15:57 +0530

    Time: 22 ms (91.21%), Space: 122.1 MB (91.21%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,98 +1,95 @@
-    class UnionFind
-    {
-        public:
-            vector<int> parent, rank;
+class UnionFind
+{
+    public:
+        vector<int> parent, rank;
 
-        UnionFind(int n)
+    UnionFind(int n)
+    {
+        parent.resize(n);
+        rank.assign(n, 0);
+
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+    }
+
+    int find(int x)
+    {
+        if (parent[x] == x)
+            return x;
+
+        return parent[x] = find(parent[x]);
+    }
+
+    void Union(int x, int y)
+    {
+        int px = find(x);
+        int py = find(y);
+
+        if (px == py)
+            return;
+
+        if (rank[px] > rank[py])
+            parent[py] = px;
+        else if (rank[px] < rank[py])
+            parent[px] = py;
+        else
         {
-            parent.resize(n);
-            rank.assign(n, 0);
+            parent[py] = px;
+            rank[px]++;
+        }
+    }
+};
+
+class Solution
+{
+    public:
+        int countCompleteComponents(int n, vector<vector < int>> &edges)
+        {
+            vector<vector < int>> components(n);
+            vector<int> degree(n, 0);
+
+            UnionFind uf(n);
+
+            for (auto &edge: edges)
+            {
+                int u = edge[0];
+                int v = edge[1];
+
+                degree[u]++;
+                degree[v]++;
+
+                uf.Union(u, v);
+            }
 
             for (int i = 0; i < n; i++)
-                parent[i] = i;
-        }
-
-        int find(int x)
-        {
-            if (parent[x] == x)
-                return x;
-
-            return parent[x] = find(parent[x]);
-        }
-
-        void Union(int x, int y)
-        {
-            int px = find(x);
-            int py = find(y);
-
-            if (px == py)
-                return;
-
-            if (rank[px] > rank[py])
-                parent[py] = px;
-            else if (rank[px] < rank[py])
-                parent[px] = py;
-            else
             {
-                parent[py] = px;
-                rank[px]++;
+                uf.find(i);
+                components[uf.parent[i]].push_back(i);
             }
-        }
-    };
-    class Solution
-    {
 
-        public:
+            int cnt = 0;
 
-            int countCompleteComponents(int n, vector<vector < int>> &edges)
+            for (int i = 0; i < n; i++)
             {
-                vector<vector < int>> components(n);
-                unordered_map<int, unordered_set < int>> mp;
-                int m = edges.size();
-                UnionFind uf(n);
-                for (int i = 0; i < m; i++)
+                if (components[i].empty())
+                    continue;
+
+                int sz = components[i].size();
+                bool complete = true;
+
+                for (int node: components[i])
                 {
-                    int u = edges[i][0];
-                    int v = edges[i][1];
-                    mp[u].insert(v);
-                    mp[v].insert(u);
-                    uf.Union(u, v);
-                }
-                for (int i = 0; i < n; i++)
-                {
-                    uf.find(i);
-                    components[uf.parent[i]].push_back(i);
-                }
-                int cnt = 0;
-                for (int i = 0; i < n; i++)
-                {
-                    if (components[i].size() == 1)
+                    if (degree[node] != sz - 1)
                     {
-                        cnt++;
-                    }
-                    else if (components[i].size() > 1)
-                    {
-                        bool complete = true;
-
-                        for (int u: components[i])
-                        {
-                            for (int v: components[i])
-                            {
-                                if (u != v && !mp[u].contains(v))
-                                {
-                                    complete = false;
-                                    break;
-                                }
-                            }
-
-                            if (!complete)
-                                break;
-                        }
-
-                        if (complete)
-                            cnt++;
+                        complete = false;
+                        break;
                     }
                 }
-                return cnt;
+
+                if (complete)
+                    cnt++;
             }
-    };
+
+            return cnt;
+        }
+};
