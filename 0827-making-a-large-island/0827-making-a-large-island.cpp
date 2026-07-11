aeb6e669f2277cbc2b302acf2ffff3ec273c24eb
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-11 14:26:07 +0530

    Time: 307 ms (19.55%), Space: 159.5 MB (34.46%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.cpp
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -1,80 +1,95 @@
-class Solution
-{
-    public:
-        int m, n;
-    vector<vector < int>> directions
-    {
-        { 1,
-            0 },
-        { -1,
-            0 },
-        { 0,
-            1 },
-        { 0,
-            -1 }
-    };
+class DSU {
+    vector<int> parent;
+    vector<int> size;
 
-    int DFS(vector<vector < int>> &grid, int i, int j, int &id)
-    {
-        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != 1)
-            return 0;
-
-        grid[i][j] = id;
-        int count = 1;
-
-        for (vector<int> &dir: directions)
-        {
-            int x = i + dir[0];
-            int y = j + dir[1];
-
-            count += DFS(grid, x, y, id);
+public:
+    DSU(int n) {
+        parent.resize(n);
+        size.resize(n, 1);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
         }
-
-        return count;
     }
-    int largestIsland(vector<vector < int>> &grid)
-    {
-        m = grid.size();
-        n = grid[0].size();
-        int maxArea = 0;
-        unordered_map<int, int> mp;
-        int island_id = 2;
 
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (grid[i][j] == 1)
-                {
-                    int size = DFS(grid, i, j, island_id);
-                    maxArea = max(maxArea, size);
-                    mp[island_id] = size;
-                    island_id++;
+    int find(int x) {
+        if (parent[x] == x)
+            return x;
+        return parent[x] = find(parent[x]); 
+    }
+
+    void Union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX == rootY) return;
+
+        if (size[rootX] > size[rootY]) {
+            parent[rootY] = rootX;
+            size[rootX] += size[rootY];
+        } else {
+            parent[rootX] = rootY;
+            size[rootY] += size[rootX];
+        }
+    }
+
+    int getSize(int x) {
+        return size[find(x)];
+    }
+};
+
+class Solution {
+public:
+    
+    vector<vector<int>> directions{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    int largestIsland(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int totalCells = n * n;
+        DSU dsu(totalCells);
+        int maxArea = 0;
+
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    int idx = i * n + j;
+                    for (auto& dir : directions) {
+                        int i_ = i + dir[0];
+                        int j_ = j + dir[1];
+                        int idx_ = i_ * n + j_;
+                        if (i_ >= 0 && i_ < n && j_ >= 0 && j_ < n && grid[i_][j_] == 1) {
+                            dsu.Union(idx, idx_);
+                        }
+                    }
                 }
             }
         }
 
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (grid[i][j] == 0)
-                {
-                    unordered_set<int> st;
-                    for (vector<int> &dir: directions)
-                    {
-                        int x = i + dir[0];
-                        int y = j + dir[1];
-                        if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] != 0)
-                            st.insert(grid[x][y]);
+        
+        for (int idx = 0; idx < totalCells; idx++) {
+            if (grid[idx / n][idx % n] == 1) {
+                maxArea = max(maxArea, dsu.getSize(idx));
+            }
+        }
+
+       
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) {
+                    unordered_set<int> uniqueParents;
+                    for (auto& dir : directions) {
+                        int i_   = i + dir[0];
+                        int j_   = j + dir[1];
+                        int idx_ = i_ * n + j_;
+                        if (i_ >= 0 && i_ < n && j_ >= 0 && j_ < n && grid[i_][j_] == 1) {
+                            uniqueParents.insert(dsu.find(idx_));
+                        }
                     }
 
-                    int sum =1;
-                        for (const int &s: st)
-                        {
-                            sum += mp[s];
-                        }
-                    maxArea = max(maxArea, sum);
+                    int newSize = 1; 
+                    for (int parent : uniqueParents) {
+                        newSize += dsu.getSize(parent);
+                    }
+                    maxArea = max(maxArea, newSize);
                 }
             }
         }
@@ -82,3 +97,4 @@ class Solution
         return maxArea;
     }
 };
+
