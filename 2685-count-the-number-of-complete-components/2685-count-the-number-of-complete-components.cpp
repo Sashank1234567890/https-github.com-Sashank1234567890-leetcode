3c93872e2a9b38411cf0eb272c40d4cc602987fb
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-11 10:41:02 +0530

    Time: 46 ms (59.1%), Space: 137.2 MB (30.36%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,46 +1,60 @@
-class Solution {
-public:
-    void dfs(int u, vector<vector<int>>& adj, vector<int>& vis,
-             int &nodes, int &edges) {
+class Solution
+{
+    public:
 
-        vis[u] = 1;
-        nodes++;
-        edges += adj[u].size();
+        int countCompleteComponents(int n, vector<vector < int>> &edges)
+        {
 
-        for(int v : adj[u]) {
-            if(!vis[v])
-                dfs(v, adj, vis, nodes, edges);
+            vector<vector < int>> adj(n);
+
+            for (auto &e: edges)
+            {
+                adj[e[0]].push_back(e[1]);
+                adj[e[1]].push_back(e[0]);
+            }
+
+            vector<int> vis(n, 0);
+
+            int ans = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+
+                if (vis[i]) continue;
+
+                queue<int> q;
+                q.push(i);
+
+                vis[i] = 1;
+
+                int nodes = 0;
+                int edgeCnt = 0;
+
+                while (!q.empty())
+                {
+
+                    int u = q.front();
+                    q.pop();
+
+                    nodes++;
+                    edgeCnt += adj[u].size();
+
+                    for (int v: adj[u])
+                    {
+                        if (!vis[v])
+                        {
+                            vis[v] = 1;
+                            q.push(v);
+                        }
+                    }
+                }
+
+                edgeCnt /= 2;
+
+                if (edgeCnt == nodes *(nodes - 1) / 2)
+                    ans++;
+            }
+
+            return ans;
         }
-    }
-
-    int countCompleteComponents(int n, vector<vector<int>>& edges) {
-
-        vector<vector<int>> adj(n);
-
-        for(auto &e : edges) {
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
-        }
-
-        vector<int> vis(n,0);
-
-        int ans=0;
-
-        for(int i=0;i<n;i++) {
-
-            if(vis[i]) continue;
-
-            int nodes=0;
-            int edgeCnt=0;
-
-            dfs(i,adj,vis,nodes,edgeCnt);
-
-            edgeCnt/=2;
-
-            if(edgeCnt==nodes*(nodes-1)/2)
-                ans++;
-        }
-
-        return ans;
-    }
 };
