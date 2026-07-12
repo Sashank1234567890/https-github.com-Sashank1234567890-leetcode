0f12d6018550a41bfc257816c061e614e976e5fc
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-12 15:50:25 +0530

    Time: 0 ms (100%), Space: 8.2 MB (42.84%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    int getsum(int n){
-        return n*(n+1)/2;
-    }
     int pivotInteger(int n) {
-        int sum=getsum(n);
-        int l=1;
-        int r=n;
-        int ans=-1;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            int x=getsum(mid);
-            if(x==sum-(x-mid))
-              return mid;
-            else if(x<sum-(x-mid))
-               l=mid+1;
-            else
-               r=mid-1;
-        }
-   return ans; }
+        if(n == 1)
+            return 1;
+        
+        int totalSum = n * (n+1)/2;
+        
+        int pivot = sqrt(totalSum);
+        
+        if(pivot*pivot == totalSum)
+            return pivot;
+        
+        return -1;
+        
+    }
 };
