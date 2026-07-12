7ad037bffafdce5064c4c13cc5c28400562307c2
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-12 11:57:36 +0530

    Time: 0 ms (100%), Space: 8.1 MB (17.33%) - LeetHub

--- 0458-poor-pigs/0458-poor-pigs.cpp
+++ 0458-poor-pigs/0458-poor-pigs.cpp
@@ -1,15 +1,9 @@
-class Solution
-{
-    public:
-        int poorPigs(int buckets, int minutesToDie, int minutesToTest)
-        {
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
 
-            int states = minutesToTest / minutesToDie + 1;
-            int pigs = 0;
+        int states = minutesToTest / minutesToDie + 1;
 
-            while (pow(states, pigs) < buckets)
-                pigs++;
-
-            return pigs;
-        }
+        return ceil(log2(buckets) / log2(states));
+    }
 };
