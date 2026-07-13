8558eb1c2cad4c5cc1b92a870e492d71d1f5110f
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-13 17:24:13 +0530

    Time: 0 ms (100%), Space: 8 MB (6.27%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -1,8 +1,14 @@
-class Solution
-{
-    public:
-        long long coloredCells(int n)
-        {
-            return (1LL + 4LL *(n - 1) + 2LL *(n - 1) *(n - 2));
+class Solution {
+public:
+    long long coloredCells(int n) {
+        int t = 1;
+        long long cells = 1;
+
+        while(t <= n) {
+            cells += (4 * (t-1));
+            t++;
         }
+
+        return cells;
+    }
 };
