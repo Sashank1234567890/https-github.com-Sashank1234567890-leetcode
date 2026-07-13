5a21503389240a8051c71770f5ec488ce0e6cee6
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-13 15:56:18 +0530

    Time: 259 ms (68.5%), Space: 355.2 MB (70.11%) - LeetHub

--- 3607-power-grid-maintenance/3607-power-grid-maintenance.cpp
+++ 3607-power-grid-maintenance/3607-power-grid-maintenance.cpp
@@ -2,60 +2,80 @@ class Solution
 {
     public:
 
-        void dfs(int node, unordered_map<int, vector < int>> &adj, int id, vector< int > &nodeId,
-            unordered_map<int, set < int>> &mp, vector< bool > &visited)
-        {
-            visited[node] = true;
-            mp[id].insert(node);
-            nodeId[node] = id;
+        vector<int> parent;
+    vector<int> rank;
 
-            for (int &ngbr: adj[node])
-            {
-                if (!visited[ngbr])
-                {
-                    dfs(ngbr, adj, id, nodeId, mp, visited);
-                }
-            }
+    int Find(int x)
+    {
+        if (x == parent[x])
+            return x;
+
+        return parent[x] = Find(parent[x]);
+    }
+
+    void Union(int x, int y)
+    {
+        int x_par = Find(x);
+        int y_par = Find(y);
+
+        if (x_par == y_par)
+            return;
+
+        if (rank[x_par] > rank[y_par])
+        {
+            parent[y_par] = x_par;
+            rank[x_par]++;
         }
+        else if (rank[x_par] < rank[y_par])
+        {
+            parent[x_par] = y_par;
+            rank[y_par]++;
+        }
+        else
+        {
+            parent[y_par] = x_par;
+            rank[x_par]++;
+        }
+    }
 
     vector<int> processQueries(int c, vector<vector < int>> &connections, vector< vector< int>> &queries)
     {
-        unordered_map<int, vector < int>> adj;
+        vector<int> result;
+        int n = connections.size();
+
+        rank.resize(c + 1, 0);
+        parent.resize(c + 1);
+        for (int i = 1; i <= c; i++)
+        {
+            parent[i] = i;
+        }
 
         for (auto &edge: connections)
         {
             int u = edge[0];
             int v = edge[1];
-
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            Union(u, v);
         }
-
-        vector<bool> visited(c + 1, false);
-        vector<int> nodeId(c + 1);
 
         unordered_map<int, set < int>> mp;
+        vector<int> nodeId(c + 1);
 
-        for (int node = 1; node <= c; node++)
+        for (int i = 1; i <= c; i++)
         {
-            if (!visited[node])
-            {
-                int id = node;
-                dfs(node, adj, id, nodeId, mp, visited);
-            }
+            int par = Find(i);
+            mp[par].insert(i);
+            nodeId[i] = par;
         }
 
-        vector<int> result;
-       	//O(q* log(c))
         for (auto &query: queries)
         {
-            int type = query[0];	//1, 2
-            int node = query[1];	
+            int type = query[0];
+            int node = query[1];
+            int id = nodeId[node];
 
             if (type == 1)
             {
-                int id = nodeId[node];
-                if (mp[id].count(node))
+                if (mp[id].find(node) != mp[id].end())
                 {
                     result.push_back(node);
                 }
@@ -70,10 +90,10 @@ class Solution
             }
             else
             {
-                int id = nodeId[node];
                 mp[id].erase(node);
             }
         }
+
         return result;
     }
 };
