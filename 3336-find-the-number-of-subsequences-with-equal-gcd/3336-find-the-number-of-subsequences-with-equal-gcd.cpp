1def8bd6e5d68a6cd6123a61512ae72585ad5fb2
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-14 11:50:49 +0530

    Time: 1135 ms (46.95%), Space: 292.2 MB (35.65%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -7,36 +7,39 @@ public:
         int maxEl = -1;
         for (int x : nums) 
             maxEl = max(maxEl, x);
-        
-        int dp[n+1][maxEl+1][maxEl+1];
 
-      
-        for (int first = 0; first <= maxEl; first++) {
-            for (int second = 0; second <= maxEl; second++) {
+        // prev = layer i+1, curr = layer i   (2D instead of 3D)
+        vector<vector<int>> prev(maxEl+1, vector<int>(maxEl+1, 0));
+
+        // Base case
+        for (int first = maxEl; first >= 0; first--) {
+            for (int second = maxEl; second >= 0; second--) {
                 bool bothNonEmpty = (first != 0 && second != 0);
                 bool gcdsMatch    = (first == second);
-                dp[n][first][second] = (bothNonEmpty && gcdsMatch) ? 1 : 0;
+                prev[first][second] = (bothNonEmpty && gcdsMatch) ? 1 : 0;
             }
         }
 
         for (int i = n - 1; i >= 0; i--) {
+            vector<vector<int>> curr(maxEl+1, vector<int>(maxEl+1, 0));
             for (int first = maxEl; first >= 0; first--) {
                 for (int second = maxEl; second >= 0; second--) {
 
-                    
-                    int skip  = dp[i + 1][first][second]; ;
+                    // Skip this index entirely
+                    int skip  = prev[first][second];
 
-                    
-                    int take1 = dp[i + 1][__gcd(first, nums[i])][second]; 
+                    // Include this index in seq1
+                    int take1 = prev[__gcd(first, nums[i])][second];
 
-                    
-                    int take2 = dp[i + 1][first][__gcd(second, nums[i])];
+                    // Include this index in seq2
+                    int take2 = prev[first][__gcd(second, nums[i])];
 
-                    dp[i][first][second] = (0LL + skip + take1 + take2) % MOD;
+                    curr[first][second] = (0LL + skip + take1 + take2) % MOD;
                 }
             }
+            prev = move(curr);
         }
- 
-        return dp[0][0][0]; 
+
+        return prev[0][0];
     }
 };
