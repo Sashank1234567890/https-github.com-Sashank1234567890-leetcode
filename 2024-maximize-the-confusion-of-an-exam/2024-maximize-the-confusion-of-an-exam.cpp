42a788540073041dfa5d25c0c00bbf16f98eb8ae
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-14 17:30:26 +0530

    Time: 21 ms (14.65%), Space: 12.6 MB (26.7%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,45 +1,23 @@
 class Solution {
 public:
     int maxConsecutiveAnswers(string answerKey, int k) {
-        int n = answerKey.length();
-        int result = 0;
-       
+        int result = k;
         
-        int i = 0, j = 0;
-        int countF = 0;
-        while(j < n) {
-            if(answerKey[j] == 'F')
-                countF++;
+        unordered_map<char, int> mp;
+
+        int i = 0;
+        for (int j = 0; j < answerKey.length(); j++) {
+
+            mp[answerKey[j]]++;
             
-            while(countF > k) {
-                if(answerKey[i] == 'F')
-                    countF--;
+            while (min(mp['T'], mp['F']) > k) {
+                mp[answerKey[i]]--;
                 i++;
             }
             
-            result = max(result, j-i+1);
-            
-            j++;
+            result = max(result, j - i + 1);
         }
-        
-        i = 0, j = 0;
-        int countT = 0;
-        while(j < n) {
-            if(answerKey[j] == 'T')
-                countT++;
-            
-            while(countT > k) {
-                if(answerKey[i] == 'T')
-                    countT--;
-                i++;
-            }
-            
-            result = max(result, j-i+1);
-            j++;
-        }
-        
         
         return result;
     }
-    
 };
