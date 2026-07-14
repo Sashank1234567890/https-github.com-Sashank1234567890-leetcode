24a461c0f3e4c0db40c3bb1a164c94f01c9c93c5
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-14 11:49:57 +0530

    Time: 898 ms (58.26%), Space: 58.3 MB (65.22%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -1,43 +1,42 @@
-class Solution
-{
-    public:
-        int mod = 1e9 + 7;
+class Solution {
+    int MOD = 1e9 + 7;
+public:
+    int subsequencePairCount(vector<int>& nums) {
+        int n = nums.size();
 
-    int dp[205][205][205];
+        int maxEl = -1;
+        for (int x : nums) 
+            maxEl = max(maxEl, x);
+        
+        int dp[n+1][maxEl+1][maxEl+1];
 
-    int cnt(int i, vector<int> &nums, int gcd1, int gcd2)
-    {
-
-        if (i == nums.size())
-        {
-            if (gcd1 == 0 || gcd2 == 0)
-                return 0;
-
-            return gcd1 == gcd2;
+      
+        for (int first = 0; first <= maxEl; first++) {
+            for (int second = 0; second <= maxEl; second++) {
+                bool bothNonEmpty = (first != 0 && second != 0);
+                bool gcdsMatch    = (first == second);
+                dp[n][first][second] = (bothNonEmpty && gcdsMatch) ? 1 : 0;
+            }
         }
 
-        if (dp[i][gcd1][gcd2] != -1)
-            return dp[i][gcd1][gcd2];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int first = maxEl; first >= 0; first--) {
+                for (int second = maxEl; second >= 0; second--) {
 
-        long long ans = 0;
+                    
+                    int skip  = dp[i + 1][first][second]; ;
 
-       	// take in first
-        ans = (ans + cnt(i + 1, nums, gcd(gcd1, nums[i]), gcd2)) % mod;
+                    
+                    int take1 = dp[i + 1][__gcd(first, nums[i])][second]; 
 
-       	// take in second
-        ans = (ans + cnt(i + 1, nums, gcd1, gcd(gcd2, nums[i]))) % mod;
+                    
+                    int take2 = dp[i + 1][first][__gcd(second, nums[i])];
 
-       	// don't take
-        ans = (ans + cnt(i + 1, nums, gcd1, gcd2)) % mod;
-
-        return dp[i][gcd1][gcd2] = ans;
-    }
-
-    int subsequencePairCount(vector<int> &nums)
-    {
-
-        memset(dp, -1, sizeof(dp));
-
-        return cnt(0, nums, 0, 0);
+                    dp[i][first][second] = (0LL + skip + take1 + take2) % MOD;
+                }
+            }
+        }
+ 
+        return dp[0][0][0]; 
     }
 };
