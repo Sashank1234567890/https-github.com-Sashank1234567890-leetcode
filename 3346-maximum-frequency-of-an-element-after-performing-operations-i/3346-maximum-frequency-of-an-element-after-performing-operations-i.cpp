bbf73589d75f27b059aa71cb5487dd60c1f895ae
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-14 21:47:56 +0530

    Time: 16 ms (86.73%), Space: 106.9 MB (90.56%) - LeetHub

--- 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
-        int maxVal = *max_element(begin(nums), end(nums)) + k;
+        int maxEl = *max_element(begin(nums), end(nums)) + k;
 
-        vector<int> diff(maxVal+2, 0);
-        unordered_map<int, int> freq;
+        vector<int> freq(maxEl + 1, 0);
 
-        for(int i = 0; i < nums.size(); i++) {
-            freq[nums[i]]++;
-
-            int l = max(nums[i]-k, 0);
-            int r = min(nums[i]+k, maxVal);
-
-            diff[l]++;
-            diff[r+1]--;
+        for(int &num : nums) {
+            freq[num]++;
         }
 
+        
+        for(int i = 1; i <= maxEl; i++) {
+            freq[i] += freq[i-1];
+        }
 
-        int result = 1;
+        int result = 0;
+        for(int target = 0; target <= maxEl; target++) {
+            if(freq[target] == 0)
+                continue;
+            
+            int leftNum  = max(0, target-k); 
+            int rightNum = min(maxEl, target+k); 
 
-        for(int target = 0; target <= maxVal; target++) {
-            diff[target] += (target > 0 ? diff[target-1] : 0);
+            int totalCount  = freq[rightNum] - (leftNum > 0 ? freq[leftNum-1] : 0);
 
-            int targetFreq     = freq[target];
-            int needConversion = diff[target] - targetFreq;
+            int targetCount = freq[target] - (target > 0 ? freq[target-1] : 0);
 
-            int maxPossibleFreq = min(needConversion, numOperations);
+            int needConversion = totalCount - targetCount;
 
-            result = max(result, targetFreq + maxPossibleFreq);
+            int maxPossibleFreq = targetCount + min(needConversion, numOperations);
+
+            result = max(result, maxPossibleFreq);
         }
 
         return result;
