d1842d4bb731b84aa327682a53f42c0f26024f09
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-15 16:16:14 +0530

    Time: 33 ms (33.33%), Space: 109.6 MB (70.44%) - LeetHub

---
+++ 3224-minimum-array-changes-to-make-differences-equal/3224-minimum-array-changes-to-make-differences-equal.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int minChanges(vector<int>& nums, int k) {
+
+        vector<int> diff(k + 2, 0);
+
+        int n = nums.size();
+
+        for (int i = 0; i < n / 2; i++) {
+
+            int a = nums[i];
+            int b = nums[n - 1 - i];
+
+            int currDiff = abs(a - b);
+
+            int minDiff = 0;
+
+            int maxDiff = max({a, b, k - a, k - b});
+
+            // initially 2 moves
+            diff[0] += 2;
+            diff[k + 1] -= 2;
+
+            // 1 move interval [minDiff ... maxDiff]
+            diff[minDiff]--;
+            diff[maxDiff + 1]++;
+
+            // 0 move at currDiff
+            diff[currDiff]--;
+            diff[currDiff + 1]++;
+        }
+
+        int ans = INT_MAX;
+        int curr = 0;
+
+        for (int d = 0; d <= k; d++) {
+
+            curr += diff[d];
+
+            ans = min(ans, curr);
+        }
+
+        return ans;
+    }
+};
