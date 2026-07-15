33da1dcfb22c34e50ceadd607f66778c68764ddb
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-15 23:09:42 +0530

    Time: 11 ms (25.08%), Space: 19.5 MB (6.04%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     bool doesAliceWin(string s) {
+        auto lambda = [](char ch) {
+            return string("aeiou").find(ch) != string::npos;
+        };
 
-        for(char &ch : s) {
-            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                return true;
-            }
-        }
-
-        return false;
+        return any_of(s.begin(), s.end(), lambda);
     }
 };
