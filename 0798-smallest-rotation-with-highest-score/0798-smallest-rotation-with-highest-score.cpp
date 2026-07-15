b609a650779862391c13346a5fd3faaffe4c2907
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-15 14:54:20 +0530

    Time: 3 ms (76.37%), Space: 78.5 MB (44.51%) - LeetHub

---
+++ 0798-smallest-rotation-with-highest-score/0798-smallest-rotation-with-highest-score.cpp
@@ -0,0 +1,58 @@
+class Solution
+{
+    public:
+        int bestRotation(vector<int> &nums)
+        {
+
+            int n = nums.size();
+
+            vector<int> diff(n + 1, 0);
+
+            for (int i = 0; i < n; i++)
+            {
+
+                int l = (i + 1) % n;
+                int r = (i - nums[i] + n) % n;
+
+                if (l <= r)
+                {
+
+                    diff[l]++;
+
+                    if (r + 1 < n)
+                        diff[r + 1]--;
+                }
+                else
+                {
+
+                    diff[0]++;
+
+                    if (r + 1 < n)
+                        diff[r + 1]--;
+
+                    diff[l]++;
+
+                    diff[n]--;
+                }
+            }
+
+            int ans = 0;
+            int score = 0;
+            int mx = -1;
+
+            for (int i = 0; i < n; i++)
+            {
+
+                score += diff[i];
+
+                if (score > mx)
+                {
+
+                    mx = score;
+                    ans = i;
+                }
+            }
+
+            return ans;
+        }
+};
