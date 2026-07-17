595f24bcdb2087b16bcd169956ed245417f2f715
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-17 16:07:53 +0530

    Time: 153 ms (29.12%), Space: 117.8 MB (24.34%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,54 +1,28 @@
 class Solution
 {
-    public:
-        int longestSubarray(vector<int> &nums, int limit)
+    public: int longestSubarray(vector<int> &nums, int limit)
+    {
+        int n = nums.size();
+
+        multiset<int> st;
+        int i = 0;
+        int j = 0;
+        int maxLength = 0;
+
+        while (j < n)
         {
-            int n = nums.size();
+            st.insert(nums[j]);
 
-            deque<int> mndq, mxdq;
-
-            int i = 0;
-            int j = 0;
-            int maxLength = 0;
-
-            while (j < n)
+            while (*st.rbegin() - *st.begin() > limit)
             {
-                int val = nums[j];
-
-                while (!mndq.empty() && nums[mndq.back()] > val)
-                {
-                    mndq.pop_back();
-                }
-
-                while (!mxdq.empty() && nums[mxdq.back()] < val)
-                {
-                    mxdq.pop_back();
-                }
-
-                mndq.push_back(j);
-                mxdq.push_back(j);
-
-                while (!mndq.empty() && !mxdq.empty() &&
-                    nums[mxdq.front()] - nums[mndq.front()] > limit)
-                {
-                    i = min(mndq.front(), mxdq.front()) + 1;
-
-                    while (!mndq.empty() && mndq.front() < i)
-                    {
-                        mndq.pop_front();
-                    }
-
-                    while (!mxdq.empty() && mxdq.front() < i)
-                    {
-                        mxdq.pop_front();
-                    }
-                }
-
-                maxLength = max(maxLength, j - i + 1);
-
-                j++;
+                st.erase(st.find(nums[i]));
+                ++i;
             }
 
-            return maxLength;
+            maxLength = max(maxLength, j - i + 1);
+            j++;
         }
+
+        return maxLength;
+    }
 };
