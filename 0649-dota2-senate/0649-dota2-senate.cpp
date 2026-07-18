11bbcd092531e197d9982c40d39bf30fdccb9975
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-18 15:39:11 +0530

    Time: 239 ms (5.01%), Space: 9.2 MB (93.77%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -1,52 +1,46 @@
 class Solution {
 public:
-
-    bool removeSenator(string &senate, char ch, int idx) {
-        bool loopAround = false;
+    int n;
+    void removeSenator(string &senate, vector<bool>& removed, char ch, int idx) {
         
         while(true) {
             
-            if(idx == 0) {
-                loopAround = true;
-            }
-            
-            if(senate[idx] == ch) {
-                senate.erase(begin(senate) + idx);
+            if(senate[idx] == ch && removed[idx] == false) {
+                removed[idx] = true;
                 break;
             }
-            
-            idx = (idx+1)%(senate.length());
-            
+            idx = (idx+1)%n;
         }
-        
-        return loopAround;
+
     }
     
     string predictPartyVictory(string senate) {
         
-        int R_Count = count(begin(senate), end(senate), 'R');
-        int D_Count = senate.length() - R_Count;
+        n = senate.length();
         
+        int R_Count = count(begin(senate), end(senate), 'R');
+        int D_Count = n - R_Count;
         
         int idx = 0;
         
+        
+        vector<bool> removed(n, false);
+        
         while(R_Count > 0 && D_Count > 0) {
             
-            if(senate[idx] == 'R') {
-                bool checkRemoval = removeSenator(senate, 'D', (idx+1)%(senate.length()));
-                D_Count--;
-                if(checkRemoval) {
-                    idx--;
-                }
-            } else {
-                bool checkRemoval = removeSenator(senate, 'R', (idx+1)%(senate.length()));
-                R_Count--;
-                if(checkRemoval) {
-                    idx--;
+            if(!removed[idx]) {
+            
+                if(senate[idx] == 'R') {
+                    removeSenator(senate, removed, 'D', (idx+1)%n);
+                    D_Count--;
+                    
+                } else {
+                    removeSenator(senate, removed, 'R', (idx+1)%n);
+                    R_Count--;
+
                 }
             }
-            
-            idx = (idx+1)%(senate.length());
+            idx = (idx+1)%n;
             
         }
         
