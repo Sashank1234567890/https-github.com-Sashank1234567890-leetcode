f303577397a92b391ea62d33c5cbd729606e7461
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-19 12:28:10 +0530

    Time: 17 ms (73.42%), Space: 68.8 MB (86.46%) - LeetHub

---
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int shortestSubarray(vector<int>& nums, int K) {
+        int N = nums.size();
+        
+        deque<int> deq;
+        vector<long long> cumulativeSum(N, 0);  // This stores the cumulative sum
+
+        int result = INT_MAX;
+        int j = 0;
+
+        // Compute cumulative sum in the cumulativeSum array using while loop
+        while (j < N) {
+            if (j == 0)
+                cumulativeSum[j] = nums[j];
+            else
+                cumulativeSum[j] = cumulativeSum[j - 1] + nums[j];
+            
+            
+            if (cumulativeSum[j] >= K) 
+                result = min(result, j + 1);
+            
+           
+            while (!deq.empty() && cumulativeSum[j] - cumulativeSum[deq.front()] >= K) {
+                result = min(result, j - deq.front()); 
+                deq.pop_front(); 
+            }
+
+           
+            while (!deq.empty() && cumulativeSum[j] <= cumulativeSum[deq.back()]) {
+                deq.pop_back(); 
+            }
+
+            
+            deq.push_back(j);
+
+            j++;  
+        }
+
+       
+        return result == INT_MAX ? -1 : result;
+    }
+};
+
+
+
