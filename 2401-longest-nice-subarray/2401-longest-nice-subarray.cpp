276f2d8d003fea3b18440ab49adaab2a3aaf3bd2
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-20 12:10:37 +0530

    Time: 0 ms (100%), Space: 61 MB (26.97%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-    bool isNice(vector<int>& nums, int start, int end) {
-        int mask = 0;
-        for (int i = start; i <= end; i++) {
-            if ((mask & nums[i]) != 0) //dekho and ke liye hume  do palce mai se ek mai zero chahiye to wrost case dek mask mean kisi na kis ke is position pe 1 hai agr ispe zero then it wil be zero for all 
-                return false;
-            mask |= nums[i];
-        }
-        return true;
-    }
-
     int longestNiceSubarray(vector<int>& nums) {
         int n = nums.size();
+
         int result = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (isNice(nums, i, j)) {
-                    result = max(result, j - i + 1);
-                } else {
-                    break; 
+        for(int i = 0; i < n; i++) {
+            int mask = 0;
+
+            for(int j = i; j < n; j++) {
+                if((mask & nums[j]) != 0) {
+                    break;
                 }
+
+                result = max(result, j-i+1);
+                mask = (mask | nums[j]);
             }
         }
 
         return result;
     }
 };
-
