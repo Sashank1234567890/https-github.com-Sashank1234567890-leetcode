8051d3391332ab92572bc1b57c750b91e809e513
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-22 20:40:58 +0530

    Time: 19 ms (57.07%), Space: 14.6 MB (77.9%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -1,36 +1,51 @@
 class Solution {
 public:
     int minFlips(string s) {
-        int n = s.size();
+        int n = s.length();
 
-        string s1, s2;
+        //khandani sliding window
+        int result = INT_MAX;
+        int flip1 = 0;
+        int flip2 = 0;
 
-        for(int i = 0; i < 2*n; i++) {
-            s1 += (i % 2 ? '1' : '0');
-            s2 += (i % 2 ? '0' : '1');
-        }
-
-        int i = 0, j = 0;
-        int result1 = 0, result2 = 0;
-        int ans = INT_MAX;
+        int i = 0;
+        int j = 0;
 
         while(j < 2*n) {
 
-            if(s[j%n] != s1[j]) result1++;
-            if(s[j%n] != s2[j]) result2++;
+            char expectedCharS1 = (j%2) ? '1' : '0';
+            char expectedCharS2 = (j%2) ? '0' : '1';
 
-            if(j - i + 1 > n) {
-                if(s[i%n] != s1[i]) result1--;
-                if(s[i%n] != s2[i]) result2--;
+            if(s[j%n] != expectedCharS1) {
+                flip1++;
+            }
+
+            if(s[j%n] != expectedCharS2) {
+                flip2++;
+            }
+
+            if(j-i+1 > n) { 
+                expectedCharS1 = (i%2) ? '1' : '0';
+                expectedCharS2 = (i%2) ? '0' : '1';
+
+                if(s[i%n] != expectedCharS1) {
+                    flip1--;
+                }
+
+                if(s[i%n] != expectedCharS2) {
+                    flip2--;
+                }
+
                 i++;
             }
 
-            if(j - i + 1 == n)
-                ans = min(ans, min(result1, result2));
+            if(j-i+1 == n) {
+                result = min({result, flip1, flip2});
+            }
 
             j++;
-        }
 
-        return ans;
+        }
+        return result;
     }
 };
