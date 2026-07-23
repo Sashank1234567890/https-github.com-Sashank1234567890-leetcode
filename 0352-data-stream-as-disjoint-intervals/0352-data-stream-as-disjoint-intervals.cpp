8020f0be60bfe09b8a8fbd0661e8e4abad091979
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-23 17:33:12 +0530

    Time: 3 ms (13.75%), Space: 11.2 MB (11.8%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
@@ -1,31 +1,53 @@
 class SummaryRanges {
+    map<int, int> intervals;
+
 public:
-    set<int>st;
-    SummaryRanges() {
-       st.clear();
-    }
-    
+    SummaryRanges() {}
+
     void addNum(int value) {
-       st.insert(value); 
-    }
-    
-    vector<vector<int>> getIntervals() {
-        vector<vector<int>> ans;
 
-        for (int x : st) {
+        int start = value;
+        int end = value;
 
-            if (ans.empty() || x > ans.back()[1] + 1) {
-                ans.push_back({x, x});
-            } else {
-                ans.back()[1] = x;
+        auto nextInterval = intervals.upper_bound(value);
+
+        if (nextInterval != intervals.begin()) {
+
+            auto prevInterval = nextInterval;
+            --prevInterval;
+
+            // value already exists
+            if (prevInterval->second >= value)
+                return;
+
+            // Merge with previous interval
+            if (prevInterval->second + 1 == value) {
+                start = prevInterval->first;
+                intervals.erase(prevInterval);
             }
         }
 
-        return ans;
+        // Merge with next interval
+        if (nextInterval != intervals.end() &&
+            nextInterval->first == value + 1) {
 
+            end = nextInterval->second;
+            intervals.erase(nextInterval);
+        }
+
+        intervals[start] = end;
+    }
+
+    vector<vector<int>> getIntervals() {
+
+        vector<vector<int>> ans;
+
+        for (auto &[start, end] : intervals)
+            ans.push_back({start, end});
+
+        return ans;
     }
 };
-
 /**
  * Your SummaryRanges object will be instantiated and called as such:
  * SummaryRanges* obj = new SummaryRanges();
