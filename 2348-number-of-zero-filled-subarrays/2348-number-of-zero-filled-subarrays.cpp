525d2a9898fc8575afab9c05bd082cb8a73bf3f5
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-23 18:02:35 +0530

    Time: 0 ms (100%), Space: 111.1 MB (99.84%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -1,35 +1,19 @@
-class Solution
-{
-    public:
-        long long zeroFilledSubarray(vector<int> &nums)
-        {
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
 
-            int n = nums.size();
-            long long ans = 0;
+        long long ans = 0;
+        int i = 0;
 
-            int i = 0;
+        for (int j = 0; j < nums.size(); j++) {
 
-            while (i < n)
-            {
-
-                if (nums[i] != 0)
-                {
-                    i++;
-                    continue;
-                }
-
-                int j = i;
-
-                while (j < n && nums[j] == 0)
-                    j++;
-
-                long long len = j - i;
-
-                ans += len *(len + 1) / 2;
-
-                i = j;
+            if (nums[j] != 0) {
+                i = j + 1;
+            } else {
+                ans += j - i + 1;
             }
-
-            return ans;
         }
+
+        return ans;
+    }
 };
