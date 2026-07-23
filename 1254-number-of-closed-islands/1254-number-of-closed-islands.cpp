cce24319f44f71a2d560899dda6e09e6e5d121b8
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-23 18:11:27 +0530

    Time: 4 ms (30.41%), Space: 13.1 MB (69.63%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,61 +1,42 @@
 class Solution {
 public:
+    
     int m, n;
-
-    int dx[4] = {-1, 1, 0, 0};
-    int dy[4] = {0, 0, -1, 1};
-
-    void dfs(vector<vector<int>>& grid, int i, int j) {
-
-        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 1)
-            return;
-
-        grid[i][j] = 1;
-
-        for (int k = 0; k < 4; k++) {
-            dfs(grid, i + dx[k], j + dy[k]);
-        }
+    bool dfs(vector<vector<int>> &grid, int r, int c) {
+        
+        if(r < 0 || r >= m || c < 0 || c >= n)
+            return false; //Not closed
+        
+        if(grid[r][c] == 1)
+            return true; //closed here
+        
+        grid[r][c] = 1;
+        
+        bool left  = dfs(grid, r, c-1);
+        bool right = dfs(grid, r, c+1);
+        bool up    = dfs(grid, r-1, c);
+        bool down  = dfs(grid, r+1, c);
+        
+        return left && right && up && down;
+        
     }
-
+    
     int closedIsland(vector<vector<int>>& grid) {
-
         m = grid.size();
         n = grid[0].size();
-
-     
-        for (int j = 0; j < n; j++) {
-
-            if (grid[0][j] == 0)
-                dfs(grid, 0, j);
-
-            if (grid[m - 1][j] == 0)
-                dfs(grid, m - 1, j);
-        }
-
-    
-        for (int i = 0; i < m; i++) {
-
-            if (grid[i][0] == 0)
-                dfs(grid, i, 0);
-
-            if (grid[i][n - 1] == 0)
-                dfs(grid, i, n - 1);
-        }
-
-        int ans = 0;
-
-       
-        for (int i = 1; i < m - 1; i++) {
-
-            for (int j = 1; j < n - 1; j++) {
-
-                if (grid[i][j] == 0) {
-                    ans++;
-                    dfs(grid, i, j);
+        
+        int count = 0;
+        
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                
+                if(grid[i][j] == 0) {
+                    count += dfs(grid, i, j);
                 }
+                
             }
         }
-
-        return ans;
+        
+        return count;
     }
 };
