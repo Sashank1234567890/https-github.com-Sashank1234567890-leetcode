aec69702f02b12530f7d1685d6ffefeee9337f81
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-23 15:15:00 +0530

    Time: 55 ms (26.6%), Space: 94.8 MB (23.36%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,56 +1,29 @@
 class Solution
 {
     public:
-
-        vector<int> buildLPS(string & s)
+        bool repeatedSubstringPattern(string s)
         {
+            int n = s.length();
 
-            int n = s.size();
-
-            vector<int> lps(n, 0);
-
-            int i = 1;
-            int len = 0;
-
-            while (i < n)
+            for (int l = 1; l <= n / 2; l++)
             {
 
-                if (s[i] == s[len])
+                if (n % l == 0)
                 {
+                    int times = n / l;
 
-                    len++;
-                    lps[i] = len;
-                    i++;
-                }
-                else
-                {
+                    string pattern = s.substr(0, l);
+                    string newStr = "";
+                    while (times--)
+                    {
+                        newStr += pattern;
+                    }
 
-                    if (len == 0)
-                    {
-                        lps[i] = 0;
-                        i++;
-                    }
-                    else
-                    {
-                        len = lps[len - 1];
-                    }
+                    if (newStr == s)
+                        return true;
                 }
             }
 
-            return lps;
+            return false;
         }
-
-    bool repeatedSubstringPattern(string s)
-    {
-
-        int n = s.size();
-
-        vector<int> lps = buildLPS(s);
-
-        int len = lps[n - 1];
-
-       	//n-len give th lonegest len repeating
-
-        return (len > 0 && n % (n - len) == 0);
-    }
 };
