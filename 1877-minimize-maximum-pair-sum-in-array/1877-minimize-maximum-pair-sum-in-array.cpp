6064e71780d10b3c34daf3bdb70fdc8920a24039
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-24 12:40:07 +0530

    Time: 181 ms (20.25%), Space: 100.1 MB (33.72%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -7,7 +7,7 @@ public:
         ranges::sort(nums);
         int ans=0;
 
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n/2;i++)
              ans=max(ans,nums[i]+nums[n-i-1]);
 
         return ans ;
