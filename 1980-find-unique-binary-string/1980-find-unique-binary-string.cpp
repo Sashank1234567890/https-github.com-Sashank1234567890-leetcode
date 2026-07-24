b9413fa837ba83fc2410672e4d6e90251f25a805
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-24 17:11:17 +0530

    Time: 4 ms (19.56%), Space: 12.7 MB (51.32%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -4,14 +4,14 @@ public:
         unordered_set<int> st;
         
         for(string &num : nums) {
-            st.insert(stoi(num, 0, 2));
+            st.insert(stoi(num, 0, 2));//coverting no to decimal in set 0 se 2^n should be
         }
         
         int n = nums.size();
         
         string result= "";
         
-        for(int number = 0; number <= 65536; number++) {
+        for(int number = 0; number <= n; number++) {
             if(st.find(number) == st.end()) {
                 result = bitset<16>(number).to_string();
                 break;
