0938f4477487fb4307836314a28f31aa9787c8bb
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-24 16:23:48 +0530

    Time: 12 ms (72.95%), Space: 15.5 MB (95.75%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -1,69 +1,33 @@
 class Solution
 {
     public:
-        bool isvowel(char x)
+        bool isVowel(char ch)
         {
-            return x == 'a' || x == 'A' || x == 'e' || x == 'E' || x == 'i' || x == 'I' || x == 'o' || x == 'O' || x == 'u' || x == 'U';
+            ch = tolower(ch);
+            return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
         }
-
-    void merge(string &s, int l, int mid, int r)
-    {
-
-        vector<char> left, right;
-
-        for (int i = l; i <= mid; i++)
-            if (isvowel(s[i]))
-                left.push_back(s[i]);
-
-        for (int i = mid + 1; i <= r; i++)
-            if (isvowel(s[i]))
-                right.push_back(s[i]);
-
-        int i = 0;
-        int j = 0;
-        vector<char> temp;
-
-        while (i < left.size() && j < right.size())
-        {
-            if (left[i] <= right[j])
-                temp.push_back(left[i++]);
-            else
-                temp.push_back(right[j++]);
-        }
-
-        while (i < left.size())
-            temp.push_back(left[i++]);
-
-        while (j < right.size())
-            temp.push_back(right[j++]);
-
-        int k = 0;
-
-        for (int i = l; i <= r; i++)
-        {
-            if (isvowel(s[i]))
-                s[i] = temp[k++];
-        }
-    }
-
-    void mergesort(string &s, int l, int r)
-    {
-
-        if (l >= r)
-            return;
-
-        int mid = l + (r - l) / 2;
-
-        mergesort(s, l, mid);
-        mergesort(s, mid + 1, r);
-
-        merge(s, l, mid, r);
-    }
 
     string sortVowels(string s)
     {
+        string temp;
+        for (char &ch: s)
+        {
+            if (isVowel(ch))
+            {
+                temp.push_back(ch);
+            }
+        }
 
-        mergesort(s, 0, s.size() - 1);
+        sort(begin(temp), end(temp));
+        int j = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (isVowel(s[i]))
+            {
+                s[i] = temp[j];
+                j++;
+            }
+        }
 
         return s;
     }
