0e33263e2c8331a3e77171075e57c8466c56cab6
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-24 19:52:17 +0530

    Time: 25 ms (15.73%), Space: 11.5 MB (22.41%) - LeetHub

--- 3988-create-grid-with-exactly-k-paths-i/3988-create-grid-with-exactly-k-paths-i.cpp
+++ 3988-create-grid-with-exactly-k-paths-i/3988-create-grid-with-exactly-k-paths-i.cpp
@@ -1,36 +1,56 @@
 class Solution
 {
     public:
-        int countPath(vector<string> &grid, int i, int j)
-        {
-            if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() ||
-                grid[i][j] == '#')
-            {
-                return 0;
-            }
-            if (i == grid.size() - 1 && j == grid[0].size() - 1 && grid[i][j] == '.') return 1;
-            int left = countPath(grid, i, j + 1);
-            int down = countPath(grid, i + 1, j);
-            return left + down;
-        }
+        vector<vector < int>> dp;
+
+    int countPath(vector<string> &grid, int i, int j)
+    {
+
+        if (i >= grid.size() || j >= grid[0].size() || grid[i][j] == '#')
+            return 0;
+
+        if (i == grid.size() - 1 && j == grid[0].size() - 1)
+            return 1;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        return dp[i][j] =
+            countPath(grid, i + 1, j) +
+            countPath(grid, i, j + 1);
+    }
+
     vector<string> createGrid(int m, int n, int k)
     {
+
         vector<string> grid(m, string(n, '.'));
+
         for (int row = 0; row < m; row++)
         {
             for (int col = 0; col < n; col++)
             {
-                if (countPath(grid, 0, 0) > k)
+
+                dp.assign(m, vector<int> (n, -1));
+                int paths = countPath(grid, 0, 0);
+
+                if (paths > k)
                 {
+
                     grid[row][col] = '#';
+
+                    dp.assign(m, vector<int> (n, -1));
+
                     if (countPath(grid, 0, 0) < k)
                         grid[row][col] = '.';
                 }
+
+                dp.assign(m, vector<int> (n, -1));
+
                 if (countPath(grid, 0, 0) == k)
                     return grid;
             }
         }
-        vector<string> em;
-        return em;
+
+        return {};
     }
 };
