e5b314cca95e97a345a3b84fcc27362249b0675c
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-24 18:59:00 +0530

    Time: 0 ms (100%), Space: 149 MB (97.36%) - LeetHub

--- 3994-minimum-adjacent-swaps-to-partition-array/3994-minimum-adjacent-swaps-to-partition-array.cpp
+++ 3994-minimum-adjacent-swaps-to-partition-array/3994-minimum-adjacent-swaps-to-partition-array.cpp
@@ -1,101 +1,39 @@
 class Solution
 {
     public:
-        const int MOD = 1e9 + 7;
-
-    long long merge(vector<int> &arr, int l, int mid, int r)
-    {
-
-        vector<int> temp;
-        int i = l;
-        int j = mid + 1;
-        long long inv = 0;
-
-        while (i <= mid && j <= r)
+        int minAdjacentSwaps(vector<int> &nums, int a, int b)
         {
 
-            if (arr[i] <= arr[j])
+            const int MOD = 1e9 + 7;
+
+            long long ans = 0;
+            long long m = 0;	// number of middle elements seen
+            long long r = 0;	// number of right elements seen
+
+            for (int x: nums)
             {
-                temp.push_back(arr[i++]);
+
+                if (x < a)
+                {
+
+                   	// All previous M and R form inversions with this L
+                    ans += m + r;
+                }
+                else if (x <= b)
+                {
+
+                   	// All previous R form inversions with this M
+                    ans += r;
+                    m++;
+                }
+                else
+                {
+
+                   	// Right element
+                    r++;
+                }
             }
-            else
-            {
-                inv += (mid - i + 1);
-                temp.push_back(arr[j++]);
-            }
+
+            return ans % MOD;
         }
-
-        while (i <= mid)
-            temp.push_back(arr[i++]);
-
-        while (j <= r)
-            temp.push_back(arr[j++]);
-
-        for (int k = l; k <= r; k++)
-            arr[k] = temp[k - l];
-
-        return inv;
-    }
-
-    long long mergeSort(vector<int> &arr, int l, int r)
-    {
-
-        if (l >= r)
-            return 0;
-
-        int mid = (l + r) / 2;
-
-        long long inv = 0;
-
-        inv += mergeSort(arr, l, mid);
-        inv += mergeSort(arr, mid + 1, r);
-        inv += merge(arr, l, mid, r);
-
-        return inv;
-    }
-
-    int minAdjacentSwaps(vector<int> &nums, int a, int b)
-    {
-
-        int n = nums.size();
-
-        vector<int> zeroPos, onePos;
-
-       	// Store original positions of each group
-        for (int i = 0; i < n; i++)
-        {
-            if (nums[i] < a)
-                zeroPos.push_back(i);
-            else if (nums[i] <= b)
-                onePos.push_back(i);
-        }
-
-        vector<int> perm(n);
-
-        int p0 = 0;
-        int p1 = 0;
-        int p2 = zeroPos.size() + onePos.size();
-
-       	// Build permutation
-        for (int i = 0; i < n; i++)
-        {
-
-            if (nums[i] < a)
-            {
-                perm[i] = p0++;
-            }
-            else if (nums[i] <= b)
-            {
-                perm[i] = zeroPos.size() + p1++;
-            }
-            else
-            {
-                perm[i] = p2++;
-            }
-        }
-
-        long long ans = mergeSort(perm, 0, n - 1);
-
-        return ans % MOD;
-    }
 };
