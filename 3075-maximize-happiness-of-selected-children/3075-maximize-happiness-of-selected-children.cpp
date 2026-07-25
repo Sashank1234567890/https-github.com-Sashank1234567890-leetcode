81acf7812c22aae28e403e3cbc5bebf71601c2e7
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-25 20:37:53 +0530

    Time: 209 ms (17.89%), Space: 113.4 MB (8.84%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -1,20 +1,32 @@
-class Solution {
-public:
-    long long maximumHappinessSum(vector<int>& happiness, int k) {
-        sort(begin(happiness), end(happiness), greater<int>());
-        
-        long long result = 0;
-        
-        int count = 0;
-        
-        for(int i = 0; i < k; i++) {
-            
-            result += max(happiness[i] - count, 0);
-            
-            count++;
-            
+class Solution
+{
+    public:
+        long long maximumHappinessSum(vector<int> &happiness, int k)
+        {
+
+            priority_queue<int, vector < int>, greater < int>> pq;
+
+            for (int x: happiness)
+            {
+                if (pq.size() < k)
+                    pq.push(x);
+                else if (x > pq.top())
+                {
+                    pq.pop();
+                    pq.push(x);
+                }
+            }
+
+            long long ans = 0;
+            int count = k - 1;
+
+            while (!pq.empty())
+            {
+                ans += max(0, pq.top() - count);
+                pq.pop();
+                count--;
+            }
+
+            return ans;
         }
-        
-        return result;
-    }
 };
