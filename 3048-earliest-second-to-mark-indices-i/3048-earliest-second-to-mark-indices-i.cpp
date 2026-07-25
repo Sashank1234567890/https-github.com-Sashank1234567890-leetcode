d0ad9422574ed4f293304e5e43e2d05d159e237b
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-25 18:04:52 +0530

    Time: 11 ms (49.18%), Space: 52.7 MB (19.67%) - LeetHub

--- 3048-earliest-second-to-mark-indices-i/3048-earliest-second-to-mark-indices-i.cpp
+++ 3048-earliest-second-to-mark-indices-i/3048-earliest-second-to-mark-indices-i.cpp
@@ -1,4 +1,5 @@
 class Solution {
+
     int m, n;
     bool isValid(int seconds, vector<int> changeIndices, vector<int> nums) {
 
@@ -10,12 +11,12 @@ class Solution {
         }
 
         for(int i = 1; i <= n; i++) {
-            if(lastPosition[i] == -1) { 
+            if(lastPosition[i] == -1) { //index i was not there in the range till seconds
                 return false;
             }
         }
 
-        map<int, int> positionIndexMap;
+        map<int, int> positionIndexMap; //so that it's kept in sorted order
         for(int i = 1; i <= n; i++) {
             int position = lastPosition[i];
             positionIndexMap[position] = i;
@@ -23,8 +24,7 @@ class Solution {
 
         int used_till = 0;
         for (auto& [position, index] : positionIndexMap) {
-
-            int req = 1 + nums[index-1]; 
+            int req = 1 + nums[index-1]; //+1 is for marking it when it becomes 0
             if (used_till + req > position) 
                 return false;
 
@@ -39,11 +39,21 @@ public:
         m = changeIndices.size();
         n = nums.size();
 
-        for (int time = 0; time < m; time++) {
-            if(isValid(time, changeIndices, nums)) 
-                return time+1; 
+        int time_left  = 0;
+        int time_right = m-1;
+        int result     = -1;
+
+        while(time_left <= time_right) {
+            int time_mid = time_left + (time_right - time_left)/2;
+
+            if(isValid(time_mid, changeIndices, nums)) {
+                result = time_mid+1;
+                time_right = time_mid-1;
+            } else {
+                time_left = time_mid+1;
+            }
         }
         
-        return -1;
+        return result;
     }
 };
