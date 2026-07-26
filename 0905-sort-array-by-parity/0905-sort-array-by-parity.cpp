1383b989e2d248c510fe020346313618cd0dde69
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-26 16:09:14 +0530

    Time: 0 ms (100%), Space: 20.5 MB (80.86%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -3,11 +3,23 @@ class Solution
     public:
         vector<int> sortArrayByParity(vector<int> &nums)
         {
-            auto lambda =[& ](int a, int b)
+
+            int i = 0;
+            int j = nums.size() - 1;
+
+            while (i < j)
             {
-                return a%2 < b%2;
-            };
-            ranges::sort(nums, lambda);
+
+                while (i < j && nums[i] % 2 == 0)
+                    i++;
+
+                while (i < j && nums[j] % 2 == 1)
+                    j--;
+
+                if (i < j)
+                    swap(nums[i], nums[j]);
+            }
+
             return nums;
         }
 };
