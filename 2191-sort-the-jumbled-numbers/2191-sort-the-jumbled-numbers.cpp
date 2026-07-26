54f0885429c4f359371fbe3af810e542f731aa37
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-26 20:00:04 +0530

    Time: 46 ms (90.14%), Space: 88.5 MB (74.83%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
 
-    string getMappedNum(string &num, vector<int>& mapping) {
-        string mappedNum = "";
-        for(int i = 0; i < num.length(); i++) {
-            char ch   = num[i];
-            mappedNum += to_string(mapping[ch-'0']);
+    int getMappedNum(int num, vector<int>& mapping) {
+        if(num < 10) {
+            return mapping[num];
         }
+
+        int mappedNum  = 0;
+        int plaveValue = 1;
+
+        while(num) {
+            int lastDigit   = num%10;
+            int mappedDigit = mapping[lastDigit];
+            mappedNum      += plaveValue * mappedDigit;
+
+            plaveValue *= 10;
+            num       /= 10;
+        }
+
         return mappedNum;
+
     }
 
     vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
@@ -15,11 +27,7 @@ public:
         vector<pair<int, int>> vec;
 
         for(int i = 0; i < n; i++) {
-            string numStr     = to_string(nums[i]);
-            
-            string mappedStr  = getMappedNum(numStr, mapping);
-
-            int mappedNum     = stoi(mappedStr);
+            int mappedNum = getMappedNum(nums[i], mapping);
 
             vec.push_back({mappedNum, i});
         }
@@ -34,4 +42,3 @@ public:
         return result;
     }
 };
-
