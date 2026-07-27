f5ddf7ae46c9ce0e4e4b15129b7f362bf391bf38
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-27 19:14:00 +0530

    Time: 0 ms (100%), Space: 7.8 MB (83.33%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
-    bool isHappy(int n) {
-        unordered_set<int>st;
-        while(1){
-            if(n==1)
-             return true;
-            if(st.find(n)!=st.end()){
-                return false;
-            }
-            st.insert(n);
-            long long sum=0;
-            while(n){
-                int d=n%10;
-                sum+=d*d;
-                n/=10;
-            } 
-             n=sum;
+    int nextNum(int n) {
+        int sum = 0;
+
+        while (n) {
+            int d = n % 10;
+            sum += d * d;
+            n /= 10;
         }
-   return false; }
+
+        return sum;
+    }
+
+    bool isHappy(int n) {
+
+        int slow = n;
+        int fast = n;
+
+        while (true) {
+            slow = nextNum(slow);
+            fast = nextNum(nextNum(fast));
+
+            if (slow == fast)
+                break;
+        }
+
+        return slow == 1;
+    }
 };
