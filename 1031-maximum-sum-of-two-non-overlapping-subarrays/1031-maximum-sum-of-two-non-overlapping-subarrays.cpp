f3a05a520b066e6a05769d3a2da63a0427ad3d45
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-28 20:37:11 +0530

    Time: 0 ms (100%), Space: 12.1 MB (82.03%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
-    int funct(vector<int>& prefSum, int L, int M) {
-        int n = prefSum.size();
-        int maxLeftSubSum = 0;
-        int result = 0; 
 
-        for(int mEnd = L + M - 1; mEnd < n; mEnd++) {
-            int lEnd = mEnd - M;
-            int lStartPrev = lEnd - L;
+    int funct(vector<int>& nums, int L, int M) {
+        int n = nums.size();
 
-            int mBlockSum = prefSum[mEnd] - prefSum[lEnd];
-            int lBlockSum = prefSum[lEnd] - (lStartPrev < 0 ? 0 : prefSum[lStartPrev]);
+        int lBlockSum = 0;
+        int mBlockSum = 0;
+
+        //starting window from index 0 to L+M-1
+        for(int i = 0; i <= L+M-1; i++) {
+            if(i < L) {
+                lBlockSum += nums[i];
+            } else {
+                mBlockSum += nums[i];
+            }
+        }
+
+        int maxLeftSubSum = lBlockSum;
+        int result = maxLeftSubSum + mBlockSum;
+
+        for(int mEnd = L + M; mEnd < n; mEnd++) {
+            lBlockSum += nums[mEnd-M] - nums[mEnd - M - L];
+            mBlockSum += nums[mEnd] - nums[mEnd - M];
 
             maxLeftSubSum = max(maxLeftSubSum, lBlockSum);
 
@@ -18,20 +29,12 @@ public:
         }
 
         return result;
+
     }
 
     int maxSumTwoNoOverlap(vector<int>& nums, int L, int M) {
         int n = nums.size();
 
-        vector<int> prefSum(n, 0);
-        prefSum[0] = nums[0];
-        for(int i = 1; i < n; i++) {
-            prefSum[i] = prefSum[i-1] + nums[i];
-        }
-        int val1=funct(prefSum, L, M);
-        if(L==M)
-             return val1;
-        return max(val1, funct(prefSum, M, L));
-
+        return max(funct(nums, L, M), funct(nums, M, L));
     }
 };
