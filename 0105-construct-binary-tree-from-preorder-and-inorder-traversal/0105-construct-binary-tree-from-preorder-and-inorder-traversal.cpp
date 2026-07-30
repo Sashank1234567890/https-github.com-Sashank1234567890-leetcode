69c49610e05dc7f012cb82959b3ea466325ae278
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-30 20:45:23 +0530

    Time: 3 ms (72.69%), Space: 27.3 MB (75.76%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -12,34 +12,33 @@
 class Solution
 {
     public:
-        TreeNode* build(vector<int> &pre, vector<int> &in, int &idx, int l, int r)
-        {
-            int n = pre.size();
-            if (idx == n)
-                return NULL;
-            int val = pre[idx];
-            int i = -1, j = 0;
-            while (j <= r)
-            {
-                if (val == in[j])
-                {
-                    i = j;
-                    break;
-                }
-                j++;
-            }
-            if (i == -1)
-                return NULL;
-            idx++;
-            TreeNode *root = new TreeNode(val);
-            root->left = build(pre, in, idx, l, i - 1);
-            root->right = build(pre, in, idx, i + 1, r);
+        unordered_map<int, int> mp;
+    TreeNode* build(vector<int> &pre, vector<int> &in, int &idx, int l, int r)
+    {
+        int n = pre.size();
+        if (idx == n)
+            return NULL;
+        int val = pre[idx];
+        if (mp.find(val) == mp.end())
+            return NULL;
 
-            return root;
-        }
+        if (mp[val] > r)
+            return NULL;
+
+        idx++;
+        TreeNode *root = new TreeNode(val);
+        root->left = build(pre, in, idx, l, mp[val] - 1);
+        root->right = build(pre, in, idx, mp[val] + 1, r);
+
+        return root;
+    }
     TreeNode* buildTree(vector<int> &pre, vector<int> &in)
     {
         int idx = 0;
+        mp.clear();
+        int i = 0;
+        for (int &x: in)
+            mp[x] = i++;
         return build(pre, in, idx, 0, pre.size() - 1);
     }
 };
