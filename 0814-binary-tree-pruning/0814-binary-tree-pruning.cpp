4b3e1e60ccf0ae4fcb9653d55c21720ed3d74896
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-07-31 19:07:08 +0530

    Time: 0 ms (100%), Space: 12.7 MB (14.14%) - LeetHub

--- 0814-binary-tree-pruning/0814-binary-tree-pruning.cpp
+++ 0814-binary-tree-pruning/0814-binary-tree-pruning.cpp
@@ -1,28 +1,42 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *  int val;
+ *  TreeNode * left;
+ *  TreeNode * right;
+ *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
-        TreeNode* pruneTree(TreeNode *root)
+      
+        bool checkOne(TreeNode *root)
         {
             if (!root)
-                return NULL;
+                return false;
 
-            root->left = pruneTree(root->left);
-            root->right = pruneTree(root->right);
+            if (root->val == 1)
+                return true;
 
-            if (!root->left && !root->right && root->val == 0)
-                return NULL;
-
-            return root;
+            return checkOne(root->left) || checkOne(root->right);
         }
+
+    TreeNode* pruneTree(TreeNode *root)
+    {
+        if (!root)
+            return NULL;
+
+        pruneTree(root->left);
+        pruneTree(root->right);
+
+        if (!checkOne(root->left)) root->left = NULL;
+        if (!checkOne(root->right)) root->right = NULL;
+
+        if (!root->left && !root->right && root->val == 0)
+            return NULL;
+        return root;
+    }
 };
