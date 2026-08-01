df3b232b1bb438cc56b78f31a6669d6109756100
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-01 16:05:55 +0530

    Time: 13 ms (38.44%), Space: 130.1 MB (43.84%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     typedef long long ll;
-
-    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
-        ll  sum        = 0;
-        int count      = 0;
-        int minloss = INT_MAX;
-
-        for (ll num : nums) {
-
-            if((num^k) > num) {
-                count++;
-                sum += (num^k);
-            } else {
-                sum += num;
-            }
-
-            minloss = min((long long)minloss, abs(num - (num ^ k)));
+    ll maximumValueSum(vector<int>& nums, int k,
+                              vector<vector<int>>& edges) {
+        vector<int> profit;
+        ll normalSum = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            profit.push_back((nums[i] ^ k) - nums[i]);
+            normalSum += 1ll * nums[i];
         }
+        
+        sort(profit.begin(), profit.end(), greater<int>());
+        
+        for (int i = 0; i < profit.size()-1; i += 2) {
 
-        if(count%2 == 0)
-            return sum;
-
-        return sum - minloss;
+            ll pairSum = profit[i] + profit[i + 1];
+            
+            if (pairSum > 0) {
+                normalSum += pairSum;
+            }
+        }
+        return normalSum;
     }
 };
