fdf19ac70d5621353a837880135c5d35c2ffc41a
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-01 09:05:41 +0530

    Time: 0 ms (100%), Space: 16.3 MB (79.97%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -1,54 +1,19 @@
 class Solution {
 public:
     int findSpecialInteger(vector<int>& arr) {
-        int x1 = 0, x2 = 0, x3 = 0;
-        int cnt1 = 0, cnt2 = 0, cnt3 = 0;
+        int n = arr.size();
 
-        for (int x : arr) {
-            if (x == x1)
-                cnt1++;
-            else if (x == x2)
-                cnt2++;
-            else if (x == x3)
-                cnt3++;
-            else if (cnt1 == 0) {
-                x1 = x;
-                cnt1 = 1;
-            }
-            else if (cnt2 == 0) {
-                x2 = x;
-                cnt2 = 1;
-            }
-            else if (cnt3 == 0) {
-                x3 = x;
-                cnt3 = 1;
-            }
-            else {
-                cnt1--;
-                cnt2--;
-                cnt3--;
-            }
+        vector<int> idx = {0, n / 4, n / 2, 3 * n / 4};
+
+        for (int i : idx) {
+            int x = arr[i];
+
+            int l = lower_bound(arr.begin(), arr.end(), x) - arr.begin();
+            int r = upper_bound(arr.begin(), arr.end(), x) - arr.begin() - 1;
+
+            if (r - l + 1 > n / 4)
+                return x;
         }
-
-        cnt1 = cnt2 = cnt3 = 0;
-
-        for (int x : arr) {
-            if (x == x1)
-                cnt1++;
-            else if (x == x2)
-                cnt2++;
-            else if (x == x3)
-                cnt3++;
-        }
-
-        int limit = arr.size() / 4;
-
-        if (cnt1 > limit)
-            return x1;
-        if (cnt2 > limit)
-            return x2;
-        if (cnt3 > limit)
-            return x3;
 
         return -1;
     }
