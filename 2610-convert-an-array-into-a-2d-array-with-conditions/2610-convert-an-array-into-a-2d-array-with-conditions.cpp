4d5e4f3686966272bdb01462e94a076bf34da5a4
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-01 11:05:17 +0530

    Time: 2 ms (65.48%), Space: 34.4 MB (51.63%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -2,36 +2,16 @@ class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
 
+        unordered_map<int, int> freq;
         vector<vector<int>> ans;
-        unordered_map<int, unordered_set<int>> mp;
 
-        for (int i = 0; i < nums.size(); i++) {
+        for (int x : nums) {
 
-            if (ans.size() == 0) {
-                ans.push_back(vector<int>());
-                ans[0].push_back(nums[i]);
-                mp[0].insert(nums[i]);
-            }
-            else {
+            if (freq[x] == ans.size())
+                ans.push_back({});
 
-                bool found = false;
-
-                for (int j = 0; j < ans.size(); j++) {
-
-                    if (!mp[j].contains(nums[i])) {
-                        ans[j].push_back(nums[i]);
-                        mp[j].insert(nums[i]);
-                        found = true;
-                        break;
-                    }
-                }
-
-                if (!found) {
-                    ans.push_back(vector<int>());
-                    ans.back().push_back(nums[i]);
-                    mp[ans.size() - 1].insert(nums[i]);
-                }
-            }
+            ans[freq[x]].push_back(x);
+            freq[x]++;
         }
 
         return ans;
