5b449163361cce0bf340bbc2b5f197a3bdaed4ee
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-02 19:11:31 +0530

    Time: 0 ms (100%), Space: 10.4 MB (68.24%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,29 +1,6 @@
-class Solution
-{
-    public:
-        bool stoneGame(vector<int> &piles)
-        {
-            int bob = 0, alice = 0;
-            int a = 1;
-            int i = 0;
-            int j = piles.size()-1;
-            while (i <= j)
-            {
-                if (a)
-                {
-                    if (piles[i] > piles[j])
-                        alice += piles[i++];
-                    else
-                        alice += piles[j--];
-                }
-                else
-                {
-                    if (piles[i] > piles[j])
-                        bob += piles[i++];
-                    else
-                        bob += piles[j--];
-                }
-            }
-            return alice > bob;
-        }
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
 };
