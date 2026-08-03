b9f52073dbfa9c7e762b3237c74ff2bf26bca635
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-03 19:23:13 +0530

    Time: 20 ms (73.83%), Space: 12.5 MB (82.42%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     int numTeams(vector<int>& rating) {
-
         int n = rating.size();
 
-        vector<vector<int>> inc(n, vector<int>(4, 0));
-        vector<vector<int>> dec(n, vector<int>(4, 0));
+        int teams = 0;
 
-        int ans = 0;
+        for(int j = 1; j < n-1; j++) {
 
-        for (int i = 0; i < n; i++) {
+            int countSmallerLeft = 0;
+            int countLargerLeft  = 0;
+            int countSmallerRight = 0;
+            int countLargerRight = 0;
 
-            inc[i][1] = 1;
-            dec[i][1] = 1;
-
-            for (int j = 0; j < i; j++) {
-
-                if (rating[j] < rating[i]) {
-
-                    inc[i][2] += inc[j][1];
-                    inc[i][3] += inc[j][2];
-                }
-
-                else if (rating[j] > rating[i]) {
-
-                    dec[i][2] += dec[j][1];
-                    dec[i][3] += dec[j][2];
+            for(int i = 0; i < j; i++) {
+                if(rating[i] < rating[j]) {
+                    countSmallerLeft++;
+                } else if(rating[i] > rating[j]) {
+                    countLargerLeft++;
                 }
             }
 
-            ans += inc[i][3];
-            ans += dec[i][3];
+            for(int k = j+1; k < n; k++) {
+                if(rating[j] < rating[k]) {
+                    countLargerRight++;
+                } else if(rating[j] > rating[k]) {
+                    countSmallerRight++;
+                }
+            }
+
+            teams += (countLargerLeft * countSmallerRight) + (countSmallerLeft * countLargerRight);
+
+
         }
 
-        return ans;
+        return teams;
     }
 };
+
