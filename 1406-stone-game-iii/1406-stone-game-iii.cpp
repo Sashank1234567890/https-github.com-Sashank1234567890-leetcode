6d70a0a83bf971ad7aca1d1f4566575f0dd30e3e
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-03 18:09:34 +0530

    Time: 39 ms (43.17%), Space: 130.2 MB (97.03%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -6,39 +6,38 @@ class Solution
 
             int n = stoneValue.size();
 
-            vector<vector < int>> dp(2, vector<int> (n + 1, 0));
+            vector<vector < int>> dp(2, vector<int> (4, 0));
 
             for (int i = n - 1; i >= 0; i--)
             {
 
-               	// Alice's turn
-                dp[1][i] = INT_MIN;
+               	// Alice
+                dp[1][i % 4] = INT_MIN;
                 int stones = 0;
 
                 for (int j = i; j < min(i + 3, n); j++)
                 {
                     stones += stoneValue[j];
-                    dp[1][i] = max(dp[1][i], stones + dp[0][j + 1]);
+                    dp[1][i % 4] = max(dp[1][i % 4],
+                        stones + dp[0][(j + 1) % 4]);
                 }
 
-               	// Bob's turn
-                dp[0][i] = INT_MAX;
+               	// Bob
+                dp[0][i % 4] = INT_MAX;
                 stones = 0;
 
                 for (int j = i; j < min(i + 3, n); j++)
                 {
                     stones -= stoneValue[j];
-                    dp[0][i] = min(dp[0][i], stones + dp[1][j + 1]);
+                    dp[0][i % 4] = min(dp[0][i % 4],
+                        stones + dp[1][(j + 1) % 4]);
                 }
             }
 
             int diff = dp[1][0];
 
-            if (diff > 0)
-                return "Alice";
-            if (diff < 0)
-                return "Bob";
-
+            if (diff > 0) return "Alice";
+            if (diff < 0) return "Bob";
             return "Tie";
         }
 };
