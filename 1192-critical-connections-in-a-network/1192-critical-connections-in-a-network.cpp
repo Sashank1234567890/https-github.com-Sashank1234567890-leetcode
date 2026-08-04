d20213a13ae993e4abc91f8ac51d2740b6c6f686
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-04 19:25:21 +0530

    Time: 188 ms (55.65%), Space: 177.5 MB (58.79%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,60 +1,53 @@
-class Solution
-{
+
+class Solution {
     public:
-        void dfs(int node, int parent, vector<vector < int>> &adj, vector< int > &vis, vector< vector< int >> &bridges, vector< int > &dist, vector< int > &low, int &timer)
-        {
+void dfs(int node, int parent, vector<vector<int>>& adj, vector<int>& vis,vector<vector<int>>&bridges, vector<int>& disc,vector<int>& low,int& timer) {
 
-            vis[node] = 1;
-            dist[node] = low[node] = timer++;
+        vis[node] = 1;
+        disc[node] = low[node] = timer++;
 
-            for (int neigh: adj[node])
-            {
-                if (neigh == parent) continue;
+        for (int neigh : adj[node]) {
+            if (neigh == parent) continue;
 
-                if (vis[neigh])
-                {
-                    low[node] = min(low[node], dist[neigh]);
-                }
-                else
-                {
+          
+            if (vis[neigh]) {
+                low[node] = min(low[node], low[neigh]);
+            }
+            else {
+             
 
-                    dfs(neigh, node, adj, vis, bridges, dist, low, timer);
+                dfs(neigh, node, adj, vis,bridges, disc, low, timer);
 
-                    low[node] = min(low[node], low[neigh]);
+                low[node] = min(low[node], low[neigh]);
 
-                    if (low[neigh] > dist[node])
-                    {
-                        vector<int> temp(2);
-                        temp[0] = node;
-                        temp[1] = neigh;
-                        bridges.push_back(temp);
-                    }
+                if (low[neigh] > disc[node]) {
+                    vector<int>temp(2);
+                    temp[0]=node;
+                    temp[1]=neigh;
+                    bridges.push_back(temp);
                 }
             }
         }
+    }
+      
+    vector<vector<int>> criticalConnections(int V, vector<vector<int>>& edges) {
+          vector<vector<int>> adj(V);
+          vector<vector<int>> bridges;
 
-    vector<vector < int>> criticalConnections(int V, vector<vector < int>> &edges)
-    {
-        vector<vector < int>> adj(V);
-        vector<vector < int>> bridges;
-
-        for (auto &e: edges)
-        {
+        for (auto &e : edges) {
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
         }
 
         vector<int> vis(V, 0);
-        vector<int> dist(V, -1);
+        vector<int> disc(V, -1);
         vector<int> low(V, -1);
 
         int timer = 0;
 
-        for (int i = 0; i < V; i++)
-        {
-            if (!vis[i])
-            {
-                dfs(i, -1, adj, vis, bridges, dist, low, timer);
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                dfs(i, -1, adj, vis,bridges, disc, low, timer);
             }
         }
 
