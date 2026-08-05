84521bb8a4134055e43fa92084b036d785ed7b0d
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-05 18:21:44 +0530

    Time: 5 ms (31.37%), Space: 18 MB (52.55%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -1,28 +1,33 @@
-class Solution {
-public:
-    int findMinDifference(vector<string>& timePoints) {
-        int n = timePoints.size();
-        vector<int> minutes(n);
+class Solution
+{
+    public:
+        int findMinDifference(vector<string> &timePoints)
+        {
+            int n = timePoints.size();
+            vector<int> minutes(n);
 
-        for(int i = 0; i < n; i++) {
-            string time = timePoints[i];
+            for (int i = 0; i < n; i++)
+            {
+                string time = timePoints[i];
 
-            string hourSubstr = time.substr(0, 2); 
-            string minSubstr = time.substr(3);
+                string hourSubstr = time.substr(0, 2);
+                string minSubstr = time.substr(3);
 
-            int hourInt = stoi(hourSubstr);
-            int minInt  = stoi(minSubstr);
+                int hourInt = stoi(hourSubstr);
+                int minInt = stoi(minSubstr);
 
-            minutes[i] = hourInt*60 + minInt;
+                minutes[i] = hourInt *60 + minInt;
+            }
+
+            sort(begin(minutes), end(minutes));
+            int result=INT_MAX;
+
+            for (int i = 0; i < n; i++)
+            {
+                int diff = (minutes[(i + 1) % n] - minutes[i] + 1440) % 1440;
+                result = min(result, diff);
+            }
+
+            return result;
         }
-
-        sort(begin(minutes), end(minutes));
-
-        int result = INT_MAX;
-        for(int i = 1; i < n; i++) {
-            result = min(result, minutes[i] - minutes[i-1]);
-        }
-
-        return min(result, (24*60 - minutes[n-1]) + minutes[0]);
-    }
 };
