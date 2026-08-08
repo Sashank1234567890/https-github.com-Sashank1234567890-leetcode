eed744afcb03032ef070226373a48008c6512019
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-08 19:56:52 +0530

    Time: 0 ms (100%), Space: 108.3 MB (15.18%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& arrays) {
-        set<pair<int,int>> st;
+        int MIN = arrays[0].front();
+        int MAX = arrays[0].back();
 
-        for (int i = 0; i < arrays.size(); i++) {
-            st.insert({arrays[i][0], i});
-            st.insert({arrays[i].back(), i});
+        int result = 0;
+
+        for(int i = 1; i < arrays.size(); i++) {
+            int currMin = arrays[i].front();
+            int currMax = arrays[i].back();
+
+            result = max({result, abs(currMin - MAX), abs(currMax - MIN)});
+
+            MAX = max(MAX, currMax);
+            MIN = min(MIN, currMin);
         }
-
-        auto mn = st.begin();
-        auto mx = prev(st.end());
-
-        if (mn->second != mx->second)
-            return mx->first - mn->first;
-
-        auto mn2 = next(mn);
-        auto mx2 = prev(mx);
-
-        return max(mx->first - mn2->first,
-                   mx2->first - mn->first);
+        return result;
     }
 };
