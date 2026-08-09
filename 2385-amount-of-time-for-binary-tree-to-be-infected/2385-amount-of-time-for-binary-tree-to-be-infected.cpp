4350e16657c5ac36c61ae6839252eb0c1071369f
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-09 15:10:41 +0530

    Time: 1 ms (99.28%), Space: 128.1 MB (95.66%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,73 +1,42 @@
-/**
- *Definition for a binary tree node.
- *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
- */
 class Solution
 {
     public:
-        void convert(TreeNode *current, int parent, unordered_map<int, vector < int>> &adj)
+        int result = INT_MIN;
+
+    int solve(TreeNode *root, int start)
+    {
+        if (root == NULL)
         {
-            if (current == nullptr)
-            {
-                return;
-            }
-
-            if (parent != -1)
-            {
-                adj[current->val].push_back(parent);
-            }
-
-            if (current->left != nullptr)
-            {
-                adj[current->val].push_back(current->left->val);
-            }
-            if (current->right != nullptr)
-            {
-                adj[current->val].push_back(current->right->val);
-            }
-            convert(current->left, current->val, adj);
-            convert(current->right, current->val, adj);
+            return 0;
         }
+
+        int LH = solve(root->left, start);
+        int RH = solve(root->right, start);
+
+        if (root->val == start)
+        {
+            result = max(LH, RH);
+            return -1;
+        }
+        else if (LH >= 0 && RH >= 0)
+        {
+            return max(LH, RH) + 1;
+        }
+        else
+        {
+            int d = abs(LH) + abs(RH);
+            result = max(result, d);
+
+            return min(LH, RH) - 1;
+        }
+
+        return 0;
+    }
 
     int amountOfTime(TreeNode *root, int start)
     {
-        unordered_map<int, vector < int>> adj;
-        convert(root, -1, adj);
+        solve(root, start);
 
-        queue<int> que;
-        que.push(start);
-        unordered_set<int> visited;
-        visited.insert(start);
-        int minutes = 0;
-
-        while (!que.empty())
-        {
-            int n = que.size();
-
-            while (n--)
-            {
-                int curr = que.front();
-                que.pop();
-
-                for (int &ngbr: adj[curr])
-                {
-                    if (visited.find(ngbr) == visited.end())
-                    {
-                        que.push(ngbr);
-                        visited.insert(ngbr);
-                    }
-                }
-            }
-            minutes++;
-        }
-
-        return minutes - 1;
+        return result;
     }
 };
