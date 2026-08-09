7d635309189b7f3cc3ab42c77597f3baa11482f6
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-09 20:48:43 +0530

    Time: 3 ms (47.64%), Space: 8.9 MB (25.85%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,13 +1,63 @@
 class Solution {
 public:
-    int solve(int n, int k) {
-        if(n == 1)
-            return 0;
 
-        return (solve(n - 1, k) + k) % n;
+    void update(vector<int>& bit, int i, int n, int val) {
+        while(i <= n) {
+            bit[i] += val;
+            i += i & -i;
+        }
     }
 
-    int findTheWinner(int n, int k) {
-        return solve(n, k) + 1;
+    int kth(vector<int>& bit, int n, int k) {
+        int pos = 0;
+
+        int p = 1;
+        while((p << 1) <= n)
+            p <<= 1;
+
+        while(p > 0) {
+            if(pos + p <= n && bit[pos + p] < k) {
+                k -= bit[pos + p];
+                pos += p;
+            }
+
+            p >>= 1;
+        }
+
+        return pos + 1;
+    }
+
+    int findTheWinner(int n, int m) {
+        vector<int> bit(n + 1, 0);
+        vector<int> ans;
+
+        int i = 1;
+
+        // Initially everyone is alive
+        while(i <= n) {
+            update(bit, i, n, 1);
+            i++;
+        }
+
+        int index = 0;
+        int left = n;
+
+        while(left >= 1) {
+
+            index = (index + m - 1) % left;
+
+            // index is 0-based
+            // Fenwick kth is 1-based
+            int person = kth(bit, n, index + 1);
+
+            ans.push_back(person);
+
+            // Remove person
+            update(bit, person, n, -1);
+
+            left--;
+        }
+
+        return ans.back();
     }
 };
