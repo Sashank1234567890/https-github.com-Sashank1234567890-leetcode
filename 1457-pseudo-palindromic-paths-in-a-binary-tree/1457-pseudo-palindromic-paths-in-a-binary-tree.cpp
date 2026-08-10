f1e01b50b76502895b55aae6619d66b8a4b3fda4
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-10 19:24:54 +0530

    Time: 17 ms (30.77%), Space: 182.1 MB (63.32%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -1,60 +1,36 @@
-/**
- *Definition for a binary tree node.
- *struct TreeNode {
- *int val;
- *TreeNode * left;
- *TreeNode * right;
- *TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
- */
-class Solution
-{
-    public:
-        void check(TreeNode *root, unordered_map<int, int> &mp, int odd, int &cnt)
-        {
-            if (!root)
-            {
-                return;
-            }
-            if (!root->left && !root->right)
-            {
-                mp[root->val]++;
-                if (mp[root->val] % 2)
-                {
-                    odd++;
-                }
-                else
-                {
-                    odd--;
-                }
-                if (odd <= 1)
-                    cnt++;
-                mp[root->val]--;
-                return;
-            }
-            mp[root->val]++;
-            if (mp[root->val] % 2)
-            {
-                odd++;
-            }
-            else
-            {
-                odd--;
-            }
+class Solution {
+public:
+    void check(TreeNode* root, unordered_map<int, int>& mp,
+               int odd, int& cnt) {
 
+        if(!root)
+            return;
+
+        mp[root->val]++;
+
+        if(mp[root->val] & 1)
+            odd++;
+        else
+            odd--;
+
+        if(!root->left && !root->right) {
+            if(odd <= 1)
+                cnt++;
+        }
+        else {
             check(root->left, mp, odd, cnt);
             check(root->right, mp, odd, cnt);
-            mp[root->val]--;
         }
-    int pseudoPalindromicPaths(TreeNode *root)
-    {
-        if (root == NULL)
-            return 0;
+
+        mp[root->val]--;
+    }
+
+    int pseudoPalindromicPaths(TreeNode* root) {
         unordered_map<int, int> mp;
         int cnt = 0;
+
         check(root, mp, 0, cnt);
+
         return cnt;
     }
 };
