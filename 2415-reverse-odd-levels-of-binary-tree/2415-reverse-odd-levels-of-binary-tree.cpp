26c7e25caf2de08ea29fec779fc31f47a68edb36
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-15 15:11:17 +0530

    Time: 3 ms (47.92%), Space: 84 MB (27.58%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
@@ -1,33 +1,39 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void solve(TreeNode* left, TreeNode* right, int l) {
-        if(!left || !right)
-            return;
-
-        if(l % 2 == 1) {
-            swap(left->val, right->val);
-        }
-
-        solve(left->left, right->right, l + 1);
-        solve(left->right, right->left, l + 1);
-    }
-
     TreeNode* reverseOddLevels(TreeNode* root) {
-        if(!root)
-            return NULL;
+        queue<TreeNode*> que;
 
-        solve(root->left, root->right, 1);
+        que.push(root);
+        int level = 0;
+
+        while(!que.empty()) {
+            int n = que.size();
+
+            vector<TreeNode*> levelNodes;
+            while(n--) {
+                TreeNode* temp = que.front();
+                que.pop();
+                levelNodes.push_back(temp);
+
+                if(temp->left) {
+                    que.push(temp->left);
+                }
+                if(temp->right) {
+                    que.push(temp->right);
+                }
+            }
+            if(level%2 == 1) {
+                int l = 0, r = levelNodes.size()-1;
+                while(l < r) {
+                    int tempVal = levelNodes[l]->val;
+                    levelNodes[l]->val = levelNodes[r]->val;
+                    levelNodes[r]->val = tempVal;
+                    l++;
+                    r--;
+                }
+            }
+            level++;
+        }
 
         return root;
     }
