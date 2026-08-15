c603febcc7d097167721f5eb7e419694dfbab1e5
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-15 15:42:21 +0530

    Time: 6 ms (86.82%), Space: 27.5 MB (78.26%) - LeetHub

--- 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.cpp
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.cpp
@@ -7,23 +7,37 @@
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ * 
  */
 class FindElements {
 public:
     unordered_set<int> st;
-    void dfs(TreeNode* root, int x) {
-        if(!root)
-            return;
-        
+    void bfs(TreeNode* root, int x) {
+        queue<TreeNode*> que;
         root->val = x;
-        st.insert(x);
-        dfs(root->left, 2*x+1);
-        dfs(root->right, 2*x+2);
+        que.push(root);
+
+        while(!que.empty()) {
+            TreeNode* temp = que.front();
+            que.pop();
+
+            st.insert(temp->val);
+
+            if(temp->left) {
+                temp->left->val = 2*temp->val + 1;
+                que.push(temp->left);
+            }
+
+            if(temp->right) {
+                temp->right->val = 2*temp->val + 2;
+                que.push(temp->right);
+            }
+            
+        }
     }
     FindElements(TreeNode* root) {
         st.clear();
-        dfs(root, 0);
+        bfs(root, 0);
     }
     
     bool find(int target) {
