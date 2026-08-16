e3b298912dcb79580f3d7ac64da8ab648aa2538a
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-16 15:55:10 +0530

    Time: 32 ms (50.25%), Space: 42.7 MB (21.29%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
-    int solve(int i, int remainder, vector<int>& nums, vector<vector<int>>& t) {
-        if (i >= nums.size()) {
-            return (remainder == 0) ? 0 : INT_MIN;
-        }
-
-        if (t[i][remainder] != -1)
-            return t[i][remainder];
-
-     
-        int pick = nums[i] +
-                   solve(i + 1, (remainder + nums[i]) % 3, nums, t);
-
-       
-        int notPick = solve(i + 1, remainder, nums, t);
-
-        return t[i][remainder] = max(pick, notPick);
-    }
-
     int maxSumDivThree(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> t(n, vector<int>(3, -1));
-        return solve(0, 0, nums, t);
+        vector<vector<int>> t(n + 1, vector<int>(3, INT_MIN));
+
+      
+        t[n][0] = 0;       // valid
+        t[n][1] = INT_MIN; 
+        t[n][2] = INT_MIN; 
+
+  
+        for (int i = n - 1; i >= 0; --i) {
+            for (int rem = 0; rem < 3; rem++) {
+
+           
+                int newRem = (rem + nums[i]) % 3;
+                int pick = (t[i + 1][newRem] == INT_MIN) 
+                           ? INT_MIN 
+                           : nums[i] + t[i + 1][newRem];
+
+        
+                int notPick = t[i + 1][rem];
+
+              
+                t[i][rem] = max(pick, notPick);
+            }
+        }
+
+        return t[0][0];
     }
 };
