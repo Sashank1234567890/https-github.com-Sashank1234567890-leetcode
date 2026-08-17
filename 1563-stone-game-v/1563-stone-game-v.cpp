1e5367e61adce3ea245806eae0ffe0fa1d54d95d
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-17 18:01:10 +0530

    Time: 1202 ms (5.7%), Space: 39.7 MB (6.55%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.cpp
+++ 1563-stone-game-v/1563-stone-game-v.cpp
@@ -2,46 +2,41 @@ class Solution {
 public:
     typedef long long ll;
 
-    vector<ll> pre;
-    vector<vector<int>> dp;
-
-    int solve(vector<int>& stoneValue, int l, int r) {
-        if(l == r)
-            return 0;
-
-        if(dp[l][r] != -1)
-            return dp[l][r];
-
-        ll ans = 0;
-
-        for(int i = l + 1; i <= r; i++) {
-            ll lsum = pre[i] - pre[l];
-            ll rsum = pre[r + 1] - pre[i];
-
-            if(rsum > lsum) {
-                ans = max(ans,lsum + solve(stoneValue, l, i - 1));
-            }
-            else if(lsum > rsum) {
-                ans = max(ans,rsum + solve(stoneValue, i, r));
-            }
-            else {
-                ans = max({ans,lsum + solve(stoneValue, l, i - 1),rsum + solve(stoneValue, i, r)});
-            }
-        }
-
-        return dp[l][r] = ans;
-    }
-
     int stoneGameV(vector<int>& stoneValue) {
         int n = stoneValue.size();
 
-        pre.assign(n + 1, 0);
+        vector<ll> pre(n + 1, 0);
 
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i < n; i++) {
             pre[i + 1] = pre[i] + stoneValue[i];
+        }
 
-        dp.assign(n, vector<int>(n, -1));
+        vector<vector<ll>> dp(n, vector<ll>(n, 0));
 
-        return solve(stoneValue, 0, n - 1);
+        for(int len = 2; len <= n; len++) {
+
+            for(int l = 0; l + len - 1 < n; l++) {
+
+                int r = l + len - 1;
+
+                for(int i = l + 1; i <= r; i++) {
+
+                    ll lsum = pre[i] - pre[l];
+                    ll rsum = pre[r + 1] - pre[i];
+
+                    if(rsum > lsum) {
+                        dp[l][r] = max(dp[l][r],lsum + dp[l][i - 1]);
+                    }
+                    else if(lsum > rsum) {
+                        dp[l][r] = max(dp[l][r],(rsum + dp[i][r]));
+                    }
+                    else {
+                        dp[l][r] = max({dp[l][r],lsum + dp[l][i - 1],rsum + dp[i][r]});
+                    }
+                }
+            }
+        }
+
+        return dp[0][n - 1];
     }
 };
