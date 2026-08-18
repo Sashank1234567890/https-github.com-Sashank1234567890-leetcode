54e465e32d8ab087b63bf7a50c112161df153ad2
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-18 21:56:26 +0530

    Time: 28 ms (60.54%), Space: 28.1 MB (45.11%) - LeetHub

--- 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.cpp
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
     string repeatLimitedString(string s, int repeatLimit) {
-        vector<int> count(26, 0); //S.C : O(26)
-        for(char &ch : s) { //T.C : O(n)
+        vector<int> count(26, 0);
+        for(char &ch : s) {
             count[ch-'a']++;
         }
 
-        string result;
-
-        int i = 25; 
-        while(i >= 0) { 
-            if(count[i] == 0) {
-                i--;
-                continue;
-            }
-
-            char ch  = 'a' + i; 
-            int freq = min(count[i], repeatLimit);
-
-            result.append(freq, ch);
-            count[i] -= freq;
-
+        priority_queue<char> pq;
+        for(int i = 0; i < 26; i++) {
             if(count[i] > 0) {
-                
-                int j = i-1;
-                while(j >= 0 && count[j] == 0) {
-                    j--;
+                char ch = 'a' + i;
+                pq.push(ch);
+            }
+        }
+
+        string result;
+        while(!pq.empty()) {
+            char ch = pq.top();
+            pq.pop();
+
+            int freq = min(count[ch-'a'], repeatLimit);
+            result.append(freq, ch);
+
+            count[ch-'a'] -= freq;
+
+            if(count[ch-'a'] > 0 && !pq.empty()) {
+                char nextChar = pq.top();
+                pq.pop();
+
+                result.push_back(nextChar);
+                count[nextChar-'a']--;
+
+                if(count[nextChar-'a'] > 0) {
+                    pq.push(nextChar);
                 }
 
-                if(j < 0) {
-                    break;
-                }
+                pq.push(ch);
 
-                result.push_back('a' + j);
-                count[j]--;
             }
         }
 
