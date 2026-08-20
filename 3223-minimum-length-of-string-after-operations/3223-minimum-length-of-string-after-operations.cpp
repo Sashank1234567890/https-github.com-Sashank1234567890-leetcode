6b5b001fea23008498502a46501a12cde4394421
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-20 11:55:25 +0530

    Time: 36 ms (42.02%), Space: 32.1 MB (53.11%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
@@ -1,45 +1,21 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        int n=s.size();
+        int n = s.length();
+        vector<int> freq(26, 0);
+        int deleted = 0;
 
-        vector<vector<int>> pos(26);
+        for(int i = 0; i < n; i++) {
+            
+            freq[s[i] - 'a'] += 1;
 
-        for(int i=0;i<n;i++)
-            pos[s[i]-'a'].push_back(i);
-
-        vector<bool> removed(n,false);
-
-        for(int i=0;i<n;i++){
-            if(removed[i])
-                continue;
-
-            int c=s[i]-'a';
-
-            int p=lower_bound(pos[c].begin(),pos[c].end(),i)-pos[c].begin();
-
-            int l=p-1;
-            int r=p+1;
-
-            while(l>=0 && removed[pos[c][l]])
-                l--;
-
-            while(r<pos[c].size() && removed[pos[c][r]])
-                r++;
-
-            if(l>=0 && r<pos[c].size()){
-                removed[pos[c][l]]=true;
-                removed[pos[c][r]]=true;
+            if(freq[s[i]-'a'] == 3) {
+                freq[s[i] - 'a'] -= 2; 
+                deleted += 2; 
             }
         }
 
-        int ans=0;
+        return n - deleted;
 
-        for(int i=0;i<n;i++){
-            if(!removed[i])
-                ans++;
-        }
-
-        return ans;
     }
 };
