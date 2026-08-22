890eb3ab071c9b9c4720bb7436adcbb4bcc3b8dc
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-22 11:32:21 +0530

    Time: 3 ms (83.93%), Space: 126.7 MB (70.49%) - LeetHub

--- 3152-special-array-ii/3152-special-array-ii.cpp
+++ 3152-special-array-ii/3152-special-array-ii.cpp
@@ -1,97 +1,34 @@
-class segTree{
-public:
-
-    struct Node{
-        bool valid;
-        bool first;
-        bool last;
-    };
-
-    vector<Node> seg;
-
-    segTree(int n){
-        seg.assign(4*n,{0,0,0});
-    }
-
-    Node build(vector<int>&arr,int i,int l,int r){
-        if(l==r){
-            seg[i]={1,(arr[l]%2==1),(arr[l]%2==1)};
-            return seg[i];
-        }
-
-        int mid=l+(r-l)/2;
-
-        auto left=build(arr,2*i+1,l,mid);
-        auto right=build(arr,2*i+2,mid+1,r);
-
-        if(!left.valid||!right.valid){
-            return seg[i]={0,left.first,right.last};
-        }
-
-        if(left.last==right.first){
-            return seg[i]={0,left.first,right.last};
-        }
-
-        return seg[i]={1,left.first,right.last};
-    }
-
-    Node query(int start,int end,int i,int l,int r){
-
-        if(l>end||r<start){
-            return {0,0,0};
-        }
-
-        if(start<=l&&r<=end){
-            return seg[i];
-        }
-
-        int mid=l+(r-l)/2;
-
-        if(end<=mid){
-            return query(start,end,2*i+1,l,mid);
-        }
-
-        if(start>mid){
-            return query(start,end,2*i+2,mid+1,r);
-        }
-
-        auto left=query(start,end,2*i+1,l,mid);
-        auto right=query(start,end,2*i+2,mid+1,r);
-
-        if(!left.valid||!right.valid){
-            return {0,left.first,right.last};
-        }
-
-        if(left.last==right.first){
-            return {0,left.first,right.last};
-        }
-
-        return {1,left.first,right.last};
-    }
-};
-
 class Solution {
 public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        int m = queries.size();
 
-        int n=nums.size();
+        vector<int> cumSum(n, 0);
+        //cumSum[i] = total count of violating indices till index i
+        cumSum[0] = 0;
 
-        segTree st(n);
-
-        st.build(nums,0,0,n-1);
-
-        vector<bool> ans;
-
-        for(auto q:queries){
-
-            int l=q[0];
-            int r=q[1];
-
-            auto x=st.query(l,r,0,0,n-1);
-
-            ans.push_back(x.valid);
+        for(int i = 1; i < n; i++) {
+            if(nums[i]%2 == nums[i-1]%2) {  
+                cumSum[i] = cumSum[i-1]+1;
+            } else {
+                cumSum[i] = cumSum[i-1];
+            }
         }
 
-        return ans;
+        vector<bool> result(m, false);
+        int i = 0;
+        for(vector<int>& query : queries) {
+            int start = query[0];
+            int end = query[1];
+
+            if(cumSum[end] - cumSum[start] == 0) {
+                result[i] = true;
+            }
+            i++;
+        }
+
+        return result;
     }
 };
+
