592bebc2436c928485542bf3aabaf33fe3183e6e
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-23 16:31:24 +0530

    Time: 51 ms (76.47%), Space: 10.2 MB (86.27%) - LeetHub

--- 3403-find-the-lexicographically-largest-string-from-the-box-i/3403-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3403-find-the-lexicographically-largest-string-from-the-box-i/3403-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
-    string answerString(string word, int numFriends) {
-        int n = word.length();
-        if( numFriends == 1)
-            return word;
+    int bestStartingPoint(string &word, int n) {
+        int i = 0; 
+        int j = 1; 
 
-        string result;
+        while(j < n) {
+            int k = 0;
 
-        int longestPossible = n - (numFriends - 1);
+            
+            while( j+k < n && word[i+k] == word[j+k]) {
+                k++;
+            }
 
-        for(int i = 0; i < n; i++) {
-            int canTakeLength = min(longestPossible, n-i);
-
-            result = max(result, word.substr(i, canTakeLength));
+            if(j+k < n && word[j+k] > word[i+k]) {
+                i = j;
+                j = j+1;
+            } else {
+                j = j + k + 1; 
+            }
         }
 
-        return result;
+        return i;
+    }
+
+    string answerString(string word, int numFriends) {
+        int n = word.length();
+
+        if(numFriends == 1)
+            return word;
+
+        int i = bestStartingPoint(word, n);
+
+        int longestPossibleLength = n - (numFriends-1);
+        int canTakePossible       = min(longestPossibleLength, n-i);
+
+        return word.substr(i, canTakePossible);
+
     }
 };
