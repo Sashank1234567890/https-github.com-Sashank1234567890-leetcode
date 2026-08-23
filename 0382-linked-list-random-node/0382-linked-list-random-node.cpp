b36b2bb42161f9a42496d2db78dc3072f12bea4c
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-23 22:14:45 +0530

    Time: 7 ms (24.48%), Space: 23.3 MB (81.11%) - LeetHub

--- 0382-linked-list-random-node/0382-linked-list-random-node.cpp
+++ 0382-linked-list-random-node/0382-linked-list-random-node.cpp
@@ -22,7 +22,7 @@ public:
         int size = 1;
         while(curr) {
             //choosing probability is 1/size (reservior sampling)
-            if(rand()%size == 0)
+            if(rand()%size < 1.0/size)
                 res = curr->val;
             curr = curr->next;
             size++;
