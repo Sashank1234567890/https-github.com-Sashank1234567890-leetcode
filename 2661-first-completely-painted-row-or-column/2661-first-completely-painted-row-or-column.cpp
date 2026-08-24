ac0ddb7e28e401557c905c326c82729a5a816b50
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-24 11:02:20 +0530

    Time: 117 ms (26.45%), Space: 161.4 MB (70.97%) - LeetHub

--- 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
@@ -1,40 +1,47 @@
 class Solution {
 public:
-    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
 
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
 
-        unordered_map<int, int> row;
-        unordered_map<int, int> col;
-
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-
-                int x = mat[i][j];
-
-                row[x] = i;
-                col[x] = j;
-            }
-        }
-
-        vector<int> rcount(m, 0);
-        vector<int> ccount(n, 0);
+        unordered_map<int, int> mp;
 
         for(int i = 0; i < arr.size(); i++) {
-
-            int x = arr[i];
-
-            int r = row[x];
-            int c = col[x];
-
-            rcount[r]++;
-            ccount[c]++;
-
-            if(rcount[r] == n || ccount[c] == m)
-                return i;
+            int val = arr[i];
+            mp[val] = i;
         }
 
-        return -1;
+        int minIndex = INT_MAX;
+
+        
+        for(int row = 0; row < m; row++) {
+            int lastIndex = INT_MIN; //last index in arr when this row will be completely painted
+
+            for(int col = 0; col < n; col++) {
+                int val = mat[row][col];
+                int idx = mp[val];
+                lastIndex = max(lastIndex, idx);
+            }
+
+            minIndex = min(minIndex, lastIndex);
+        }
+
+   
+        for(int col = 0; col < n; col++) {
+            int lastIndex = INT_MIN; //last index in arr when this col will be completely painted
+
+            for(int row = 0; row < m; row++) {
+                int val = mat[row][col];
+                int idx = mp[val];
+                lastIndex = max(lastIndex, idx);
+            }
+
+            minIndex = min(minIndex, lastIndex);
+        }
+        
+
+        return minIndex;
+
     }
 };
