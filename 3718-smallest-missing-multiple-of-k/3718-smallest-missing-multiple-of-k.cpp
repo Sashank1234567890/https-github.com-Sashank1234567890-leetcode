fff87ec7adf1c8e53d5922be2079d110f7061fc9
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-25 09:19:05 +0530

    Time: 4 ms (23.25%), Space: 25.1 MB (24.36%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,21 +1,17 @@
-class Solution
-{
-    public:
-        int missingMultiple(vector<int> &nums, int k)
-        {
-            ranges::sort(nums);
-            int t=k;
-            for (int x: nums)
-            {
-                if (x > t)
-                {
-                    return t;
-                }
-                else if (x == t)
-                {
-                    t+=k;
-                }
-            }
-            return t;
-        }
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+
+        unordered_set<int> st;
+
+        for(int x : nums)
+            st.insert(x);
+
+        int t = k;
+
+        while(st.count(t))
+            t += k;
+
+        return t;
+    }
 };
