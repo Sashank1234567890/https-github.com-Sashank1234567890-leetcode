4ae53119afbae88ef080a2f6348fcb92b4efa164
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-25 10:23:42 +0530

    Time: 0 ms (100%), Space: 63.5 MB (81.42%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -4,26 +4,25 @@ public:
 
         int n = prices.size();
 
-        int t[50001][2];
-
-        t[n][0] = 0;
-        t[n][1] = 0;
+        int buy = 0;
+        int sell = 0;
 
         for(int day = n - 1; day >= 0; day--) {
 
-            // buy
-            int consider = t[day + 1][0] - prices[day];
-            int not_consider = t[day + 1][1];
+            int newBuy = max(
+                sell - prices[day],
+                buy
+            );
 
-            t[day][1] = max(consider, not_consider);
+            int newSell = max(
+                buy + prices[day] - fee,
+                sell
+            );
 
-            // sell
-            consider = t[day + 1][1] + prices[day] - fee;
-            not_consider = t[day + 1][0];
-
-            t[day][0] = max(consider, not_consider);
+            buy = newBuy;
+            sell = newSell;
         }
 
-        return t[0][1];
+        return buy;
     }
 };
