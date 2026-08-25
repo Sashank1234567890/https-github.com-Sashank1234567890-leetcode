a212bd9285f0a8f1e64eb89f0a20bdb6eaa06add
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-25 16:23:29 +0530

    Time: 321 ms (59.26%), Space: 85.1 MB (44.18%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
-        int totalNumberOfTuples = 0;
         int n = nums.size();
+        int tuples = 0;
 
-        int result = 0;
-        sort(begin(nums), end(nums));
+        unordered_map<int, int> mp; 
 
-        
-        for (int i = 0; i < n; i++) {           
-            for (int j = n - 1; j > i; j--) {   
-
-                int product = nums[i] * nums[j];
-                unordered_set<int> st;
-
-                for (int k = i+1; k < j; k++) {  
-                    
-                    if(product % nums[k] == 0) {
-                        int lValue = product/nums[k];
-
-                        if(st.count(lValue)) {
-                            totalNumberOfTuples++;
-                        }
-
-                        st.insert(nums[k]);
-                    }
-                }
+        for(int i = 0; i < n; i++) {
+            for(int j = i+1; j < n; j++) {
+                int p = nums[i] * nums[j];
+                mp[p]++;
             }
         }
 
-     
-        return totalNumberOfTuples * 8;
+        for(auto &it : mp) {
+            int prod = it.first;
+            int freq = it.second;
+
+            tuples += (freq * (freq-1))/2;
+        }
+
+        return tuples * 8;
     }
 };
-
