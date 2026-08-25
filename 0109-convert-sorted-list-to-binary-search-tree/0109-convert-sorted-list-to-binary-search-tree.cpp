cca293959f56af9c4e221f53c36d010e5c1a6719
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-25 22:45:54 +0530

    Time: 4 ms (67.13%), Space: 33.5 MB (13.16%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -1,50 +1,27 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* sortedListToBST(ListNode* head) {
-        if(!head)
+    TreeNode* solve(vector<int>& arr, int l, int r) {
+        if(l > r)
             return NULL;
-        if(!head->next)
-            return new TreeNode(head->val);
-        
-   
-        ListNode* slow = head;
-        ListNode* fast = head;
-        ListNode* slow_prev = NULL;
-        while(fast && fast->next) {
-            slow_prev = slow;
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        
-        
-        TreeNode* root = new TreeNode(slow->val); 
-        slow_prev->next = NULL; 
-        
-        root->left  = sortedListToBST(head);       
-        root->right = sortedListToBST(slow->next);
-        
+
+        int mid = l + (r - l) / 2;
+
+        TreeNode* root = new TreeNode(arr[mid]);
+
+        root->left = solve(arr, l, mid - 1);
+        root->right = solve(arr, mid + 1, r);
+
         return root;
-        
+    }
+
+    TreeNode* sortedListToBST(ListNode* head) {
+        vector<int> arr;
+
+        while(head) {
+            arr.push_back(head->val);
+            head = head->next;
+        }
+
+        return solve(arr, 0, arr.size() - 1);
     }
 };
