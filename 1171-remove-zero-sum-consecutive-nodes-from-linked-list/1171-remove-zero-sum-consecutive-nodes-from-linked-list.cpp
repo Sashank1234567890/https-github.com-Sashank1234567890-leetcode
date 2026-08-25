83bdff88d2823e9a026364a05c3807a871e640cb
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-25 23:16:55 +0530

    Time: 1 ms (68.57%), Space: 16.1 MB (70.69%) - LeetHub

--- 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.cpp
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -1,55 +1,31 @@
-/**
- *Definition for singly-linked list.
- *struct ListNode {
- *    int val;
- *    ListNode * next;
- *    ListNode() : val(0), next(nullptr) {}
- *    ListNode(int x) : val(x), next(nullptr) {}
- *    ListNode(int x, ListNode *next) : val(x), next(next) {}
- *};
- */
-class Solution
-{
-    public:
-        ListNode* removeZeroSumSublists(ListNode *head)
-        {
-            int prefixSum = 0;
-            unordered_map<int, ListNode*> mp;
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
 
-            ListNode *dummy = new ListNode(0);
-            dummy->next = head;
-            mp[0] = dummy;
+        unordered_map<int, ListNode*> mp;
 
-            while (head)
-            {
-                prefixSum += head->val;
+        int sum = 0;
+        ListNode* curr = dummy;
 
-                if (mp.find(prefixSum) != mp.end())
-                {
-
-                    ListNode *P = mp[prefixSum];
-                    ListNode *start = P;
-                    int pSum = prefixSum;
-
-                    while (start != head)
-                    {
-
-                        start = start->next;
-                        pSum += start->val;
-                        if (start != head)
-                            mp.erase(pSum);
-                    }
-
-                    P->next = start->next;
-                }
-                else
-                {
-                    mp[prefixSum] = head;
-                }
-
-                head = head->next;
-            }
-
-            return dummy->next;
+        // First pass: store the last node for each prefix sum
+        while (curr) {
+            sum += curr->val;
+            mp[sum] = curr;
+            curr = curr->next;
         }
+
+        // Second pass: skip zero-sum sequences
+        sum = 0;
+        curr = dummy;
+
+        while (curr) {
+            sum += curr->val;
+            curr->next = mp[sum]->next;
+            curr = curr->next;
+        }
+
+        return dummy->next;
+    }
 };
