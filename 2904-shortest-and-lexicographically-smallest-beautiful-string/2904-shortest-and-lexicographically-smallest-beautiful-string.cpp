34255f23886532758e2bcb1e168d1f000cea6165
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-26 09:06:36 +0530

    Time: 0 ms (100%), Space: 8.4 MB (98.99%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -3,47 +3,49 @@ class Solution
     public:
         string shortestBeautifulSubstring(string s, int k)
         {
-            string ans = "";
             int sidx = -1;
             int i = 0, j = 0;
             int slen = INT_MAX;
             int cnt = 0;
             int n = s.length();
+
             while (j < n)
             {
                 cnt += (s[j] == '1');
+
                 while (i < j && cnt > k)
                 {
                     cnt -= (s[i] == '1');
                     i++;
                 }
+
                 while (i <= j && cnt == k)
                 {
                     int len = j - i + 1;
+
                     if (len < slen)
-                    {   
+                    {
                         slen = len;
                         sidx = i;
-                        // cout<<sidx<<" ";
                     }
                     else if (len == slen)
                     {
-                        string s1 = s.substr(sidx, slen);
-                        string s2 = s.substr(i, len);
-                        if (s2 < s1)
+                        if (s.compare(i, len, s, sidx, slen) < 0)
                         {
                             sidx = i;
                         }
                     }
+
                     cnt -= (s[i] == '1');
                     i++;
-                   
                 }
+
                 j++;
             }
+
             if (slen == INT_MAX)
                 return "";
-                // cout<<sidx<<" ";
+
             return s.substr(sidx, slen);
         }
 };
