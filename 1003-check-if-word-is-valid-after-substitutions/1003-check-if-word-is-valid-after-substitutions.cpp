675b74f1b9f41100f8c23df751c0e0b6f66e4daf
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-28 13:00:59 +0530

    Time: 0 ms (100%), Space: 10.4 MB (98.18%) - LeetHub

---
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int i=0,j=0;
+        while(j<s.size()){
+            s[i++]=s[j++];
+            if(i>=3&&s[i-1]=='c'&&s[i-2]=='b'&&s[i-3]=='a')
+            i-=3;
+        }
+        s.erase(begin(s)+i,end(s));
+        return s=="";
+    }
+};
