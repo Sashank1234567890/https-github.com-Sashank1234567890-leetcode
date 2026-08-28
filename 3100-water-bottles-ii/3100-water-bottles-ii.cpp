14eb78de706415324aa049722099d011e08ffc7a
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-28 16:43:22 +0530

    Time: 0 ms (100%), Space: 8.4 MB (97.78%) - LeetHub

--- 3100-water-bottles-ii/3100-water-bottles-ii.cpp
+++ 3100-water-bottles-ii/3100-water-bottles-ii.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
-    int maxBottlesDrunk(int val,int cost) {
-        int cnt=0,empty=0;
-        while(val){
-              cnt+=val;
-             empty+=val;
-              val=0;
-              while(empty>=cost){
-                val++;
-                empty-=cost;
-                cost++;
-              }
-        }
-   return cnt; }
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        return numBottles + ((-2*numExchange + 3 + sqrt(4*numExchange*numExchange+8*numBottles - 12 * numExchange + 1))/2);
+    }
 };
