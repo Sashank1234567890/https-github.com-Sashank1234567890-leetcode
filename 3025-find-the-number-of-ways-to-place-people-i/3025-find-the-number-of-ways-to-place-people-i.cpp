c9ff816b4ac82bbf6ed6920eb2b5c9d20bb8209b
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-28 14:45:33 +0530

    Time: 1 ms (79.91%), Space: 32.7 MB (28.31%) - LeetHub

--- 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.cpp
+++ 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.cpp
@@ -4,6 +4,19 @@ class Solution
         int numberOfPairs(vector<vector < int>> &points)
         {
             int n = points.size();
+
+           	// Sort: x ascending, if x same then sort as y descending
+            auto lambda =[](vector<int> &point1, vector<int> &point2)
+            {
+                if (point1[0] == point2[0])
+                {
+                    return point1[1] > point2[1];
+                }
+                return point1[0] < point2[0];
+            };
+
+            sort(points.begin(), points.end(), lambda);
+
             int result = 0;
 
             for (int i = 0; i < n; i++)
@@ -11,36 +24,22 @@ class Solution
                 int x1 = points[i][0];
                 int y1 = points[i][1];
 
-                for (int j = 0; j < n; j++)
-                {
-                    if (i == j) continue;
+                int bestY = INT_MIN;
 
+                for (int j = i + 1; j < n; j++)
+                {
                     int x2 = points[j][0];
                     int y2 = points[j][1];
 
-                    if (x1 <= x2 && y1 >= y2)
+                    if (y2 > y1)
                     {
-                        bool hasPointInside = false;
+                        continue;
+                    }
 
-                        for (int k = 0; k < n; k++)
-                        {
-                            if (k == i || k == j) continue;
-
-                            int x3 = points[k][0];
-                            int y3 = points[k][1];
-
-                            if (x3 >= x1 && x3 <= x2 &&
-                                y3 <= y1 && y3 >= y2)
-                            {
-                                hasPointInside = true;
-                                break;
-                            }
-                        }
-
-                        if (!hasPointInside)
-                        {
-                            result++;
-                        }
+                    if (y2 > bestY)
+                    {
+                        result++;
+                        bestY = y2;
                     }
                 }
             }
