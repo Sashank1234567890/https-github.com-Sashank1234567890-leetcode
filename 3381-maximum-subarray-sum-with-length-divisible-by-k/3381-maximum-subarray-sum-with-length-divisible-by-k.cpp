9808d0adbf10a4ac8805879fc42ba92b194a7594
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-08-29 15:47:04 +0530

    Time: 0 ms (100%), Space: 168.6 MB (47.21%) - LeetHub

--- 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -1,30 +1,37 @@
-class Solution
-{
+class Solution {
 public:
-    long long maxSubarraySum(vector<int>& nums, int k)
-    {
-        unordered_map<int, long long> mp;
+    long long maxSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
 
-        long long pre = 0;
-        long long ans = LLONG_MIN;
+        vector<long long> prefSum(n);
+        prefSum[0] = nums[0];
 
-        mp[0] = 0;
-
-        for(int i = 0; i < nums.size(); i++)
-        {
-            pre += nums[i];
-
-            int rem = (i + 1) % k;
-
-            if(mp.contains(rem))
-            {
-                ans = max(ans, pre - mp[rem]);
-                mp[rem] = min(mp[rem], pre);
-            }
-            else
-                mp[rem] = pre;
+        for(int i = 1; i < n; i++) {
+            prefSum[i] = prefSum[i-1] + nums[i];
         }
 
-        return ans;
+        long long result = LLONG_MIN;
+
+        for(int start = 0; start < k; start++) {
+            long long currSum = 0;
+
+            int i = start;
+
+            while(i < n && i + k - 1 < n) {
+                int j = i + k - 1;
+
+                long long subSum = prefSum[j] - ((i > 0) ? prefSum[i-1] : 0);
+
+                currSum = max(subSum, currSum + subSum);
+
+                result = max(result, currSum);
+
+                i += k;
+            }
+        }
+
+        return result;
     }
 };
+
+
