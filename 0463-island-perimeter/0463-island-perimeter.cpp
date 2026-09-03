fb19ebb0368fe19befd68dd6dce42805aad1f9a0
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-09-03 17:56:07 +0530

    Time: 3 ms (68.98%), Space: 102.3 MB (37.54%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,30 +1,51 @@
-class Solution {
-public:
-    int islandPerimeter(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        
-        int perimeter = 0;
-        for(int i = 0; i<m; i++) {
-            for(int j = 0; j<n; j++) {
-                if(grid[i][j] == 0)
-                    continue;
-                
-                if(i-1 < 0 || grid[i-1][j] == 0) 
-                    perimeter++;
-                
-                if(i+1 >= m || grid[i+1][j] == 0) 
-                    perimeter++;
-                
-                if(j-1 < 0 || grid[i][j-1] == 0) 
-                    perimeter++;
-                
-                if(j+1 >= n || grid[i][j+1] == 0) 
-                    perimeter++;
-                
+class Solution
+{
+    public:
+        int m;
+    int n;
+    int peri;
+
+    void dfs(vector<vector < int>> &grid, int i, int j)
+    {
+
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0)
+        {
+            peri++;
+            return;
+        }
+
+        if (grid[i][j] == -1)
+        {
+            return;
+        }
+
+        grid[i][j] = -1;
+
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+
+    int islandPerimeter(vector<vector < int>> &grid)
+    {
+        m = grid.size();
+        n = grid[0].size();
+        peri = 0;
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+
+                if (grid[i][j] == 1)
+                {
+                    dfs(grid, i, j);
+                    return peri;
+                }
             }
         }
-        
-        return perimeter;
+
+        return -1;
     }
 };
