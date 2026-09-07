cd0a7bfa932a03f75ba37a8bc772bf8e03047df0
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-09-07 18:54:31 +0530

    Time: 0 ms (100%), Space: 10.6 MB (9.52%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -1,26 +1,8 @@
-
 class Solution {
 public:
     int M = 1e9+7;
     int dp[2001];
     vector<int> prev; //prev[n] = last time when we saw this nth character (1-based indexing)
-
-    int solve(int n) {
-        if(n == 0)
-            return 1;
-
-        if(dp[n] != -1)
-            return dp[n];
-        
-        int total = (2*solve(n-1)) % M;
-
-        if(prev[n] != 0) {
-            int duplicates = solve(prev[n] - 1);
-            total = (total - duplicates + M) % M;
-        }
-
-        return dp[n] = total;
-    }
 
     int distinctSubseqII(string s) {
         int n = s.length();
@@ -36,8 +18,20 @@ public:
             lastSeen[idx] = i;
         }
 
-        return (solve(n) - 1 + M) % M;
+        dp[0] = 1; 
+
+        for(int i = 1; i <= n; i++) {
+            int total = (2 * dp[i-1]) % M;
+
+            if(prev[i] != 0) {
+                int duplicates = dp[prev[i] - 1];
+                total = (total - duplicates + M) % M;
+            }
+
+            dp[i] = total;
+        }
+
+        return (dp[n] - 1 + M) % M;
 
     }
 };
-
