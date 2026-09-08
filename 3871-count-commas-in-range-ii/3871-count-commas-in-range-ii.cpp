5c0c8b70db81541c301a4399976bcdbe1f36dd8f
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-09-08 16:21:50 +0530

    Time: 0 ms (100%), Space: 8.9 MB (99.22%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,41 +1,16 @@
+#define ll long long
 class Solution {
 public:
-    long long power(long long n, long long k) {
-        if(k == 0)
-            return 1;
-
-        long long x = power(n, k / 2);
-
-        long long ans = x * x;
-
-        if(k & 1)
-            ans *= n;
-
-        return ans;
-    }
-
     long long countCommas(long long n) {
-        if(n <= 999)
-            return 0;
+        ll result = 0;
 
-        long long k = 0;
-        long long d = n;
+        ll start = 1000;
 
-        while(d > 999) {
-            k++;
-            d /= 1000;
+        while(start <= n) {
+            result += (n - start + 1);
+            start *= 1000;
         }
 
-        long long ans = 0;
-
-        while(k > 0) {
-            long long limit = power(1000, k);
-
-            ans += n - limit + 1;
-
-            k--;
-        }
-
-        return ans;
+        return result;
     }
 };
