d25d5efb36b96aeefe9c2bfffb51cda695ac79ef
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-09-08 21:28:39 +0530

    Time: 230 ms (71.03%), Space: 66.3 MB (55.51%) - LeetHub

--- 4040-minimum-operations-to-form-subset-sum-i/4040-minimum-operations-to-form-subset-sum-i.cpp
+++ 4040-minimum-operations-to-form-subset-sum-i/4040-minimum-operations-to-form-subset-sum-i.cpp
@@ -1,115 +1,74 @@
 class Solution {
 public:
-
-    int n;
-    int target;
-
-    const int INF = 1e9;
-
-    vector<vector<int>> dp;
-
-
-    int solve(vector<int>& nums, int i, int sum)
-    {
-        // Required sum achieved
-        if (sum == 0)
-            return 0;
-
-        // Cannot form negative sum
-        if (sum < 0)
-            return INF;
-
-        // No elements left
-        if (i == n)
-            return INF;
-
-        if (dp[i][sum] != -1)
-            return dp[i][sum];
-
-
-        // Don't take nums[i]
-        int skip = solve(nums, i + 1, sum);
-
-
-        // Take nums[i] after multiplication
-        int takeMultiply = INF;
-
-        int val = nums[i];
-        int cost = 0;
-
-        while (val <= sum)
-        {
-            int next = solve(
-                nums,
-                i + 1,
-                sum - val
-            );
-
-            if (next != INF)
-            {
-                takeMultiply = min(
-                    takeMultiply,
-                    cost + next
-                );
-            }
-
-            // Prevent integer overflow
-            if (val > sum / 2)
-                break;
-
-            val *= 2;
-            cost++;
-        }
-
-
-        // Take nums[i] after division
-        int takeDivide = INF;
-
-        val = nums[i];
-        cost = 0;
-
-        while (val > 0)
-        {
-            int next = solve(
-                nums,
-                i + 1,
-                sum - val
-            );
-
-            if (next != INF)
-            {
-                takeDivide = min(
-                    takeDivide,
-                    cost + next
-                );
-            }
-
-            val /= 2;
-            cost++;
-        }
-
-
-        return dp[i][sum] =
-            min({
-                skip,
-                takeMultiply,
-                takeDivide
-            });
-    }
-
-
     int minOperations(vector<int>& nums, int sum)
     {
-        n = nums.size();
-        target = sum;
+        int n = nums.size();
+        const int INF = 1e9;
 
-        dp.assign(
-            n,
-            vector<int>(sum + 1, -1)
+        vector<vector<int>> dp(
+            n + 1,
+            vector<int>(sum + 1, INF)
         );
 
-        int ans = solve(nums, 0, sum);
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = 0;
 
-        return ans == INF ? -1 : ans;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int s = 1; s <= sum; s++)
+            {
+                int skip = dp[i + 1][s];
+
+                int takeMultiply = INF;
+
+                int val = nums[i];
+                int cost = 0;
+
+                while (val <= s)
+                {
+                    if (dp[i + 1][s - val] != INF)
+                    {
+                        takeMultiply = min(
+                            takeMultiply,
+                            cost + dp[i + 1][s - val]
+                        );
+                    }
+
+                    if (val > s / 2)
+                        break;
+
+                    val *= 2;
+                    cost++;
+                }
+
+                int takeDivide = INF;
+
+                val = nums[i];
+                cost = 0;
+
+                while (val > 0)
+                {
+                    if (val <= s &&
+                        dp[i + 1][s - val] != INF)
+                    {
+                        takeDivide = min(
+                            takeDivide,
+                            cost + dp[i + 1][s - val]
+                        );
+                    }
+
+                    val /= 2;
+                    cost++;
+                }
+
+                dp[i][s] = min({
+                    skip,
+                    takeMultiply,
+                    takeDivide
+                });
+            }
+        }
+
+        return dp[0][sum] == INF ? -1 : dp[0][sum];
     }
 };
