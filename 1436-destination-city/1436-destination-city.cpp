1df711bc679ee1064d3d742fbcd65092a8ca4355
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-09-12 09:18:17 +0530

    Time: 1 ms (56.75%), Space: 15 MB (63.54%) - LeetHub

--- 1436-destination-city/1436-destination-city.cpp
+++ 1436-destination-city/1436-destination-city.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
-        unordered_map<string,bool>mp;
-        unordered_set<string>st;
+        unordered_set<string> st;
+
         for(auto &path:paths){
-            string u=path[0];
-            string v=path[1];
-            if(mp.find(v)==mp.end()){
-               st.insert(v);
-            }
-            if(st.count(u)){
-                st.erase(u);
-            }
-            mp[u]=1;
+            st.insert(path[1]);
         }
-        
+
+        for(auto &path:paths){
+            st.erase(path[0]);
+        }
+
         return *st.begin();
     }
 };
