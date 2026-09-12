2d1aff18d5afd9c45a5dd07bb1d0174285b19f5c
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-09-12 21:12:12 +0530

    Time: 2178 ms (22.82%), Space: 384.4 MB (18.48%) - LeetHub

--- 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
+++ 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
@@ -1,76 +1,96 @@
 class Solution
 {
     public:
-        vector<vector < long long>> dp;
-    vector<vector<vector< int>>> best;
+        int n;
+    vector<int> nextIdx;
+
+    struct Node
+    {
+        long long score = -1;
+        vector<int> idxs;
+    };
+
+    vector<vector < Node>> t;
+
+    int findNext(vector<vector < int>> &intervals, int r)
+    {
+        int lo = 0, hi = n - 1;
+        int result = n;
+        while (lo <= hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+            if (intervals[mid][0] > r)
+            {
+                result = mid;
+                hi = mid - 1;
+            }
+            else
+            {
+                lo = mid + 1;
+            }
+        }
+        return result;
+    }
+
+    Node solve(vector<vector < int>> &intervals, int i, int k)
+    {
+        if (k == 0 || i >= n)
+            return Node();
+
+        if (t[i][k].score != -1)
+            return t[i][k];
+
+        int weight = intervals[i][2];
+        int idx = intervals[i][3];
+        int j = nextIdx[i];
+
+      
+        Node skip = solve(intervals, i + 1, k);
+
+     
+        Node temp = solve(intervals, j, k - 1);
+        Node take;
+        take.score = temp.score + weight;
+        take.idxs = temp.idxs;
+        take.idxs.push_back(idx);
+        sort(begin(take.idxs), end(take.idxs));
+
+        Node result;
+        if (skip.score > take.score)
+        {
+            result = skip;
+        }
+        else if (skip.score < take.score)
+        {
+            result = take;
+        }
+        else
+        {
+            result = (skip.idxs < take.idxs) ? skip : take;
+        }
+
+        return t[i][k] = result;
+    }
 
     vector<int> maximumWeight(vector<vector < int>> &intervals)
     {
-        int n = intervals.size();
-        vector<vector < int>> time;
+        n = intervals.size();
 
         for (int i = 0; i < n; i++)
-        {
-            time.push_back({ intervals[i][0],
-                intervals[i][1],
-                intervals[i][2],
-                i });
-        }
+            intervals[i].push_back(i);
 
-        sort(time.begin(), time.end());
+        sort(intervals.begin(), intervals.end());
 
-        vector<int> nxt(n);
-
+        nextIdx.resize(n);
         for (int i = 0; i < n; i++)
         {
-            int l = i + 1, r = n;
-
-            while (l < r)
-            {
-                int mid = l + (r - l) / 2;
-
-                if (time[mid][0] > time[i][1])
-                    r = mid;
-                else
-                    l = mid + 1;
-            }
-
-            nxt[i] = l;
+            int r = intervals[i][1];
+            nextIdx[i] = findNext(intervals, r);
         }
 
-        dp.assign(5, vector < long long > (n + 1, 0));
-        best.assign(5, vector<vector < int>> (n + 1));
+        const int K = 4;
+        t.assign(n + 1, vector<Node> (K + 1));
 
-        for (int cnt = 3; cnt >= 0; cnt--)
-        {
-            for (int i = n - 1; i >= 0; i--)
-            {
-                long long skip = dp[cnt][i + 1];
-                vector<int> skipAns = best[cnt][i + 1];
-
-                long long take = time[i][2] + dp[cnt + 1][nxt[i]];
-
-                vector<int> takeAns;
-                takeAns.push_back(time[i][3]);
-
-                for (int &x: best[cnt + 1][nxt[i]])
-                    takeAns.push_back(x);
-
-                sort(takeAns.begin(), takeAns.end());
-
-                if (take > skip || (take == skip && takeAns < skipAns))
-                {
-                    dp[cnt][i] = take;
-                    best[cnt][i] = takeAns;
-                }
-                else
-                {
-                    dp[cnt][i] = skip;
-                    best[cnt][i] = skipAns;
-                }
-            }
-        }
-
-        return best[0][0];
+        return solve(intervals, 0, K).idxs;
     }
 };
