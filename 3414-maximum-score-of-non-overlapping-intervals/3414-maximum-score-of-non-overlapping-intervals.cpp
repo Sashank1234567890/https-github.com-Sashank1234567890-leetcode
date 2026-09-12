7db7f4d097eac611f1040ce066fb0a93d12d1b14
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-09-12 08:45:59 +0530

    Time: 1062 ms (36.95%), Space: 363.2 MB (23.91%) - LeetHub

--- 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
+++ 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
@@ -1,77 +1,32 @@
-class Solution {
-public:
+class Solution
+{
+    public:
+        vector<vector < long long>> dp;
+    vector<vector<vector< int>>> best;
 
-    vector<vector<long long>> dp;
-    vector<vector<vector<int>>> best;
-    vector<int> nxt;
-
-    long long solve(int cnt, int i, vector<vector<int>>& time) {
-
-        int n = time.size();
-
-        if (cnt == 4 || i == n) {
-            best[cnt][i].clear();
-            return 0;
-        }
-
-        if (dp[cnt][i] != -1)
-            return dp[cnt][i];
-
-        // Skip
-        long long skip = solve(cnt, i + 1, time);
-        vector<int> skipAns = best[cnt][i + 1];
-
-        // Take
-        long long take =
-            time[i][2] + solve(cnt + 1, nxt[i], time);
-
-        vector<int> takeAns;
-        takeAns.push_back(time[i][3]);
-
-        for (int x : best[cnt + 1][nxt[i]])
-            takeAns.push_back(x);
-
-        sort(takeAns.begin(), takeAns.end());
-
-        if (take > skip ||
-            (take == skip && takeAns < skipAns)) {
-
-            best[cnt][i] = takeAns;
-            return dp[cnt][i] = take;
-        }
-
-        best[cnt][i] = skipAns;
-        return dp[cnt][i] = skip;
-    }
-
-    vector<int> maximumWeight(vector<vector<int>>& intervals) {
-
+    vector<int> maximumWeight(vector<vector < int>> &intervals)
+    {
         int n = intervals.size();
+        vector<vector < int>> time;
 
-        vector<vector<int>> time;
-
-        for (int i = 0; i < n; i++) {
-
-            time.push_back({
-                intervals[i][0],
+        for (int i = 0; i < n; i++)
+        {
+            time.push_back({ intervals[i][0],
                 intervals[i][1],
                 intervals[i][2],
-                i
-            });
+                i });
         }
 
         sort(time.begin(), time.end());
 
-        // next non-overlapping interval
-        nxt.resize(n);
+        vector<int> nxt(n);
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
+            int l = i + 1, r = n;
 
-            int l = i + 1;
-            int r = n;
-
-            while (l < r) {
-
+            while (l < r)
+            {
                 int mid = l + (r - l) / 2;
 
                 if (time[mid][0] > time[i][1])
@@ -83,14 +38,38 @@ public:
             nxt[i] = l;
         }
 
-        dp.assign(5, vector<long long>(n, -1));
+        dp.assign(5, vector < long long > (n + 1, 0));
+        best.assign(5, vector<vector < int>> (n + 1));
 
-        best.resize(5);
+        for (int cnt = 3; cnt >= 0; cnt--)
+        {
+            for (int i = n - 1; i >= 0; i--)
+            {
+                long long skip = dp[cnt][i + 1];
+                vector<int> skipAns = best[cnt][i + 1];
 
-        for (int i = 0; i < 5; i++)
-            best[i].resize(n + 1);
+                long long take = time[i][2] + dp[cnt + 1][nxt[i]];
 
-        solve(0, 0, time);
+                vector<int> takeAns;
+                takeAns.push_back(time[i][3]);
+
+                for (int &x: best[cnt + 1][nxt[i]])
+                    takeAns.push_back(x);
+
+                sort(takeAns.begin(), takeAns.end());
+
+                if (take > skip || (take == skip && takeAns < skipAns))
+                {
+                    dp[cnt][i] = take;
+                    best[cnt][i] = takeAns;
+                }
+                else
+                {
+                    dp[cnt][i] = skip;
+                    best[cnt][i] = skipAns;
+                }
+            }
+        }
 
         return best[0][0];
     }
