f92e65e73d0db16f10bb7b4160175040053304a7
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-09-13 17:17:34 +0530

    Time: 0 ms (100%), Space: 15.2 MB (23.86%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     vector<int> lexicalOrder(int n) {
-       deque<int>q;
-       for(int i=1;i<=9;i++){
-        q.push_back(i);
-       } 
-       vector<int>ans;
-       while(!q.empty()){
-        int x=q.front();
-        q.pop_front();
-        if(x>n)
-        continue;
-        ans.push_back(x);
-        for(int i=9;i>=0;i--){
-            q.push_front(x*10+i);
+
+        deque<int> q;
+
+        for (int i = 1; i <= 9 && i <= n; i++) {
+            q.push_back(i);
         }
-       }
-   return ans; }
+
+        vector<int> ans;
+
+        while (!q.empty()) {
+
+            int x = q.front();
+            q.pop_front();
+
+            ans.push_back(x);
+
+            for (int i = 9; i >= 0; i--) {
+
+                int child = x * 10 + i;
+
+                if (child <= n) {
+                    q.push_front(child);
+                }
+            }
+        }
+
+        return ans;
+    }
 };
