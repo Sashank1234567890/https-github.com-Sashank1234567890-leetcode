020371e83105f4d675fcced13086917fa9ebb702
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-09-14 18:17:08 +0530

    Time: 799 ms (39.34%), Space: 495.1 MB (8.2%) - LeetHub

---
+++ 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+    public:
+        int mod = 1e9 + 7;
+    int lengthAfterTransformations(string s, int t)
+    {
+        vector<int> freq(26, 0);
+        for (char &x: s)
+        {
+            freq[x - 'a'] = (freq[x - 'a'] + 1) % mod;
+        }
+
+        while (t--)
+        {
+            vector<int> temp(26, 0);
+            for (int i = 0; i < 25; i++)
+            {
+                temp[(i + 1)] = (freq[i]) % mod;
+            }
+            temp[1] = (temp[1] + freq[25]) % mod;
+            temp[0] = (freq[25])%mod;
+            freq = temp;
+        }
+        int cnt = 0;
+        for (int i = 0; i < 26; i++)
+        {
+            cnt = (cnt+freq[i])%mod;
+        }
+        return cnt;
+    }
+};
