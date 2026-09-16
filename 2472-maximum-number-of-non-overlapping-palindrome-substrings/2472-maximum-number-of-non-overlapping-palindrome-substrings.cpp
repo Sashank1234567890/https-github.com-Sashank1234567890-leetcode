43b5f3b0b57458dc39bfe97205a256cd5cf38cf1
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-09-16 18:45:43 +0530

    Time: 286 ms (20.41%), Space: 19.7 MB (35.71%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
     vector<vector<bool>> isPalindrome;
+    vector<int> t;
 
-    int solve(const string& s, int k, int i, int j, vector<vector<int>>& t) {
-        int n = s.size();
-        if (i >= n || j >= n) 
-            return 0;
-            
-        if (t[i][j] != -1) 
-            return t[i][j];
+    int solve(int n, int k) {
+        if (n < k) return 0;
 
-        if (isPalindrome[i][j]) { 
-            int growWindow  = solve(s, k, i, j + 1, t);
-            int takeIt      = 1 + solve(s, k, j + 1, j + k, t);
-            int slideWindow = solve(s, k, i + 1, j + 1, t);
+        if (t[n] != -1) 
+            return t[n];
 
-            return t[i][j] = max({growWindow, takeIt, slideWindow});
+        int result = solve(n - 1, k); 
+
+        int j = n - 1;
+       
+        for (int i = 0; j-i+1>=k; i++) {
+            if (isPalindrome[i][j]) {
+                result = max(result, 1 + solve(i, k));
+            }
         }
 
-        int slideWindow = solve(s, k, i + 1, j + 1, t);
-        int growWindow  = solve(s, k, i, j + 1, t);
-
-        return t[i][j] = max(slideWindow, growWindow);
+        return t[n] = result;
     }
 
     int maxPalindromes(string s, int k) {
-        int n = s.size();
-        if (k == 1)
-            return n; 
+        int n = s.length();
+        isPalindrome.assign(n, vector<bool>(n, false));
 
-        isPalindrome.assign(n+1, vector<bool>(n+1));
-   
-        for(int L = 1; L <= n; L++) {
-            for(int i = 0; i+L <= n; i++) {
+        for (int L = 1; L <= n; L++) {
+            for (int i = 0; i + L <= n; i++) {
                 int j = i + L - 1;
 
-                if(i == j) {
-                    isPalindrome[i][i] = true; 
-                } else if(i+1 == j) {
+                if (i == j) {
+                    isPalindrome[i][i] = true;
+                } else if (i + 1 == j) {
                     isPalindrome[i][j] = (s[i] == s[j]);
                 } else {
                     isPalindrome[i][j] = ((s[i] == s[j]) && isPalindrome[i+1][j-1] == true);
@@ -45,9 +40,7 @@ public:
             }
         }
 
-        vector<vector<int>> t(n, vector<int>(n, -1));
-
-        return solve(s, k, 0, k - 1, t);
+        t.assign(n + 1, -1);
+        return solve(n, k);
     }
 };
-
