afaed5e8535849df802d0bdd249f584fce1637ab
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-09-16 17:59:25 +0530

    Time: 891 ms (9.34%), Space: 26.7 MB (45.33%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     int mod=1e9+7;
     int count(int n,int k,vector<vector<int>>&dp){
-        if(n==0){
-            return k==0;
-        }
-        if(k<0)
+        if(k==0)
+        return 1;
+        if(n<0)
         return 0;
         if(dp[n][k]!=-1)
         return dp[n][k];
@@ -23,7 +22,20 @@ public:
         if(k==n-1){
             return 1;
         }
-        vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
-        return count(n-1,k,dp);
+        vector<vector<int>>dp(n,vector<int>(k+1,-1));
+        // dp[0][0]=1;
+        // for(int i=1;i<=n-1;i++){
+        //     for(int j=1;j<=k;j++){
+        //         for(int l=i-1;l>=0;l--){
+        //             if(l<j-1){
+        //                 break;
+        //             }
+        //             dp[i][j]=(dp[i][j]+dp[l][j-1])%mod;
+        //         }
+        //          dp[i][j]=(dp[i][j]+dp[i-1][j])%mod;
+        //     }
+        // }
+
+        return dp[n-1][k]=count(n-1,k,dp);
     }
 };
