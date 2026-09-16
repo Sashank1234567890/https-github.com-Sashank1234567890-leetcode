c74401ec9e7a32d594a7343adbe8b6bbea4c8a91
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-09-16 18:27:19 +0530

    Time: 1534 ms (7.65%), Space: 208.3 MB (17.09%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,41 +1,40 @@
-class Solution {
-public:
-    bool isPalindrome(const string& s, int i, int j) {
-        while(i < j) {
-            if(s[i++] != s[j--])
-                return false;
-        }
-        return true;
-    }
-
-    int solve(const string& s, int k, int i, int j, vector<vector<int>>& t) {
-        int n = s.size();
-
-        if(i >= n || j >= n)
-            return 0;
-
-        if(t[i][j] != -1)
-            return t[i][j];
-
-        int growWindow = solve(s, k, i, j + 1, t);
-        int slideWindow = solve(s, k, i + 1, j + 1, t);
-        int takeIt = 0;
-
-        if(isPalindrome(s, i, j)) {
-            takeIt = 1 + solve(s, k, j + 1, j + k, t);
+class Solution
+{
+    public:
+        bool isPalindrome(const string &s, int i, int j)
+        {
+            while (i < j)
+            {
+                if (s[i++] != s[j--]) return false;
+            }
+            return true;
         }
 
-        return t[i][j] = max({growWindow, slideWindow, takeIt});
-    }
-
-    int maxPalindromes(string s, int k) {
+    int maxPalindromes(string s, int k)
+    {
         int n = s.size();
+        if (k == 1)
+            return n;	
 
-        if(k == 1)
-            return n;
+        vector<vector < int>> t(n + 1, vector<int> (n + 1, 0));
 
-        vector<vector<int>> t(n, vector<int>(n, -1));
+        for (int i = n - 1; i >= 0; --i)
+        {
+            for (int j = n - 1; j >= i; --j)
+            {
 
-        return solve(s, k, 0, k - 1, t);
+                if (isPalindrome(s, i, j))
+                {
+                    int takeIt = 1 + (j + k <= n ? t[j + 1][j + k] : 0);
+
+                    t[i][j] = max(takeIt,t[i][j]);
+                }
+
+                int slideWindow = t[i + 1][j + 1];
+                int growWindow = t[i][j + 1];
+                t[i][j] = max({ t[i][j],slideWindow,growWindow });}
+        }
+
+        return t[0][k - 1];
     }
 };
