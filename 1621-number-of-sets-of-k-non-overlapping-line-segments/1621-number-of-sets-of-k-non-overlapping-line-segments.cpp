91ff8f1222fbaf6dc8f279ea2c20b8794e416d28
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-09-16 18:19:06 +0530

    Time: 43 ms (58.88%), Space: 34.6 MB (39.25%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -11,17 +11,19 @@ public:
 
         for (int k = 1; k <= K; k++) {
 
+            vector<int> prevRowSum(n + 1, 0);
+
+            //prevRowSum[x] = dp[k-1][x] + dp[k-1][x+1] + ... + dp[k-1][n-1]
+            for (int x = n - 1; x >= 0; x--) {
+                prevRowSum[x] = (prevRowSum[x + 1] + dp[k - 1][x]) % M;
+            }
+
             for (int i = n - 1; i >= 0; i--) {
                 
                 int skip = dp[k][i + 1];
 
-                int take = 0;
-                for (int j = i + 1; j < n; j++) {
-                    if(n-j<k-1){
-                        break;
-                    }
-                    take = (take + dp[k - 1][j]) % M;
-                }
+                int take = prevRowSum[i+1];
+                //dp[k-1][i+1] + dp[k-1][i+2] ...... + dp[k-1][n-1]
 
                 dp[k][i] = (take + skip) % M;
             }
