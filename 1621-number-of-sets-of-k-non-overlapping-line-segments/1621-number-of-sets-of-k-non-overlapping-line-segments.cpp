4cc86942980248b9eb11394e7c0386e04ae91f77
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-09-16 18:09:52 +0530

    Time: 2646 ms (5.14%), Space: 12.2 MB (67.76%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
-    int mod=1e9+7;
-    int count(int n,int k,vector<vector<int>>&dp){
-        if(k==0)
-        return 1;
-        if(n<0)
-        return 0;
-        if(dp[n][k]!=-1)
-        return dp[n][k];
-        int cnt=0;
-        for(int i=n-1;i>=0;i--){
-            if(i<k-1){
-                break;
-            }
-            cnt=(cnt+count(i,k-1,dp))%mod;
-        }
-        cnt=(cnt+count(n-1,k,dp))%mod;
-        return  dp[n][k]=cnt;
-    }
-    int numberOfSets(int n, int k) {
-        if(k==n-1){
-            return 1;
-        }
-        vector<vector<int>>dp(n,vector<int>(k+1,-1));
-        // dp[0][0]=1;
-        // for(int i=1;i<=n-1;i++){
-        //     for(int j=1;j<=k;j++){
-        //         for(int l=i-1;l>=0;l--){
-        //             if(l<j-1){
-        //                 break;
-        //             }
-        //             dp[i][j]=(dp[i][j]+dp[l][j-1])%mod;
-        //         }
-        //          dp[i][j]=(dp[i][j]+dp[i-1][j])%mod;
-        //     }
-        // }
+    int M = 1e9 + 7;
+    int dp[1001][1001];
 
-        return dp[n-1][k]=count(n-1,k,dp);
+    int numberOfSets(int n, int K) {
+
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = (i < n) ? 1 : 0;
+        }
+
+        for (int k = 1; k <= K; k++) {
+
+            for (int i = n - 1; i >= 0; i--) {
+                
+                int skip = dp[k][i + 1];
+
+                int take = 0;
+                for (int j = i + 1; j < n; j++) {
+                    if(n-j<k-1){
+                        break;
+                    }
+                    take = (take + dp[k - 1][j]) % M;
+                }
+
+                dp[k][i] = (take + skip) % M;
+            }
+        }
+
+        return dp[K][0];
     }
 };
