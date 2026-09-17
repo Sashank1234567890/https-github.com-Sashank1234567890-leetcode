f79282b5df5f6fa6435c820e06cd4f5b97eb30cf
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-09-17 21:42:56 +0530

    Time: 33 ms (61.7%), Space: 75.3 MB (18.43%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,55 +1,99 @@
-class Solution
-{
-    public:
+class Solution {
+public:
 
-        int partition(vector<int> &nums, int l, int h)
-        {
+    int R = 5;
 
-            // int randomIndex = l + rand() % (h - l + 1);
-            // swap(nums[l], nums[randomIndex]);
+    // 3-way partition
+    void partition_algo(vector<int>& nums, int L, int R, int P,
+                        int& low, int& high) {
 
-            int pivot = nums[l];
-            int i = l + 1;
-            int j = h;
+        int i = L;
+        low = L;
+        high = R;
 
-            while (i <= j)
-            {
+        while (i <= high) {
 
-                while (i <= h && nums[i] >= pivot)
-                    i++;
-
-                while (j >= l + 1 && nums[j] < pivot)
-                    j--;
-
-                if (i < j)
-                    swap(nums[i], nums[j]);
+            if (nums[i] > P) {
+                swap(nums[i], nums[low]);
+                i++;
+                low++;
             }
 
-            swap(nums[l], nums[j]);
-            return j;
+            else if (nums[i] < P) {
+                swap(nums[i], nums[high]);
+                high--;
+            }
+
+            else {
+                i++;
+            }
         }
-
-    int quickSelect(vector<int> &nums, int l, int h, int k)
-    {
-
-        if (l == h)
-            return nums[l];
-
-        int idx = partition(nums, l, h);
-
-        if (idx == k - 1)
-            return nums[idx];
-
-        if (idx > k - 1)
-            return quickSelect(nums, l, idx - 1, k);
-
-        return quickSelect(nums, idx + 1, h, k);
     }
 
-    int findKthLargest(vector<int> &nums, int k)
-    {
 
-        srand(time(0));
+    // Median of Medians
+    int MOM(vector<int>& nums, int L, int R) {
+
+        int n = R - L + 1;
+
+        // If at most 5 elements
+        if (n <= 5) {
+            sort(nums.begin() + L, nums.begin() + R + 1);
+
+            return nums[L + (n - 1) / 2];
+        }
+
+        vector<int> medians;
+
+        // Divide into groups of 5
+        for (int i = L; i <= R; i += 5) {
+
+            int end = min(i + 4, R);
+
+            // Sort current group
+            sort(nums.begin() + i, nums.begin() + end + 1);
+
+            // Store median
+            int mid = i + (end - i) / 2;
+
+            medians.push_back(nums[mid]);
+        }
+
+        // Find median of medians
+        return MOM(medians, 0, medians.size() - 1);
+    }
+
+
+    int quickSelect(vector<int>& nums, int L, int R, int k) {
+
+        // Choose pivot using Median of Medians
+        int P = MOM(nums, L, R);
+
+        int low, high;
+
+        // Partition:
+        // [ > P ][ == P ][ < P ]
+        partition_algo(nums, L, R, P, low, high);
+
+        // k-1 is required 0-based position
+        int target = k - 1;
+
+        // Required element is in greater-than part
+        if (target < low) {
+            return quickSelect(nums, L, low - 1, k);
+        }
+
+        // Required element is equal to pivot
+        if (target <= high) {
+            return P;
+        }
+
+        // Required element is in smaller-than part
+        return quickSelect(nums, high + 1, R, k);
+    }
+
+
+    int findKthLargest(vector<int>& nums, int k) {
 
         return quickSelect(nums, 0, nums.size() - 1, k);
     }
