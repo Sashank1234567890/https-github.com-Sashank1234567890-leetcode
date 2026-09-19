7d63b73904cc733d654d1535e00114012dd813fa
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-09-19 16:08:05 +0530

    Time: 0 ms (100%), Space: 63.6 MB (48.61%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -3,15 +3,17 @@ public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int left = 0;
         int right = arr.size() - 1;
-        while(left < right) {
+        int mid = left + (right - left) / 2;
+        while(left <= right) {
 
-            int mid = left + (right - left) / 2;
+            
             if(arr[mid]<arr[mid+1])
             left=mid+1;
-            else{ right=mid;
+            else{ right=mid-1;
             }
+             mid = left + (right - left) / 2;
              
             
         }
-    return right;}
+    return mid;}
 };
