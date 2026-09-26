055cf6f04e2dce6c00b9e246ecfc471b357b1b59
Sashank1234567890 <97720539+Sashank1234567890@users.noreply.github.com>
2026-09-26 14:11:29 +0530

    Time: 27 ms (78.5%), Space: 75.2 MB (18.04%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -2,59 +2,101 @@ class Solution
 {
 public:
 
-    pair<int, int> partition_algo(vector<int> &arr, int L, int R)
+    // 3-way partition
+    void partition_algo(vector<int>& nums, int L, int R, int P,
+                        int& low, int& high)
     {
-        int p = arr[R];
+        int i = L;
+        low = L;
+        high = R;
 
-        int l = L, m = L, h = R;
-
-        while(m <= h)
+        while(i <= high)
         {
-            if(arr[m] > p)
+            if(nums[i] > P)
             {
-                swap(arr[l], arr[m]);
-                l++;
-                m++;
+                swap(nums[i], nums[low]);
+                i++;
+                low++;
             }
-            else if(arr[m] < p)
+            else if(nums[i] < P)
             {
-                swap(arr[m], arr[h]);
-                h--;
+                swap(nums[i], nums[high]);
+                high--;
             }
             else
             {
-                m++;
+                i++;
             }
         }
-
-        return {l, h};
     }
 
-    int quickSelect(vector<int> &nums, int L, int R, int k)
+    // Median of Medians
+    int MOM(vector<int>& nums, int L, int R)
     {
-        if(L >= R)
+        int n = R - L + 1;
+
+        // Base case
+        if(n <= 5)
+        {
+            sort(nums.begin() + L, nums.begin() + R + 1);
+
+            return nums[L + (n - 1) / 2];
+        }
+
+        vector<int> medians;
+
+        // Divide into groups of 5
+        for(int i = L; i <= R; i += 5)
+        {
+            int end = min(i + 4, R);
+
+            sort(nums.begin() + i, nums.begin() + end + 1);
+
+            int mid = i + (end - i) / 2;
+
+            medians.push_back(nums[mid]);
+        }
+
+        // Median of medians
+        return MOM(medians, 0, medians.size() - 1);
+    }
+
+    int quickSelect(vector<int>& nums, int L, int R, int k)
+    {
+        // Base case
+        if(L == R)
             return nums[L];
 
-        auto [lt, gt] = partition_algo(nums, L, R);
+        // Pivot using Median of Medians
+        int P = MOM(nums, L, R);
 
-        int leftSize = lt - L;
-        int equalSize = gt - lt + 1;
+        int low, high;
 
-        if(k <= leftSize)
+        // [ > P ][ == P ][ < P ]
+        partition_algo(nums, L, R, P, low, high);
+
+        int target = k - 1;
+
+        // k-th largest lies in greater part
+        if(target < low)
         {
-            return quickSelect(nums, L, lt - 1, k);
+            return quickSelect(nums, L, low - 1, k);
         }
-        else if(k <= leftSize + equalSize)
+
+        // k-th largest is equal to pivot
+        else if(target <= high)
         {
-            return nums[lt];
+            return P;
         }
+
+        // k-th largest lies in smaller part
         else
         {
-            return quickSelect(nums, gt + 1, R,k - leftSize - equalSize);
+            return quickSelect(nums, high + 1, R, k);
         }
     }
 
-    int findKthLargest(vector<int> &nums, int k)
+    int findKthLargest(vector<int>& nums, int k)
     {
         return quickSelect(nums, 0, nums.size() - 1, k);
     }
